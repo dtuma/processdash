4d6ce0b2822abd23ed9bda3347f444f2c077a73a
David Tuma <processdash@tuma-solutions.com>
2001-03-22 05:48:49 +0000

    Add new formatting capability.  Add sorting capability.
    git-svn-id: https://svn.code.sf.net/p/processdash/code/trunk@203 560cad82-bde2-470f-af40-e4e1f8d6548a

--- pspdash/data/ResultSet.java
+++ pspdash/data/ResultSet.java
@@ -34,13 +34,20 @@ import java.util.*;
 
 public class ResultSet {
 
+    protected String prefix[];
+    protected String suffix[];
+    protected double multiplier[];
     protected Object[][] data;
 
     /** Create a result set to store the given number of rows and columns
      * of data. */
-    protected ResultSet(int numRows, int numCols) {
+    public ResultSet(int numRows, int numCols) {
         // Add a header row and a header column
         data = new Object[numRows+1][numCols+1];
+        multiplier = new double[numCols+1];
+        prefix = new String[numCols+1];
+        suffix = new String[numCols+1];
+        for (int i=numCols;  i >= 0;  i--) setFormat(i, null);
     }
 
     /** Return a new ResultSet which is the transposition of this one. */
@@ -48,7 +55,7 @@ public class ResultSet {
         int row = numRows(), col = numCols();
         ResultSet result = new ResultSet(col, row);
         for (;  row >= 0;  row--)
-            for (;  col >= 0;  col--)
+            for (col=numCols();  col >= 0;  col--)
                 result.data[col][row] = data[row][col];
         return result;
     }
@@ -61,7 +68,7 @@ public class ResultSet {
     public int numRows() { return data.length - 1; }
     /** Change the header name of a given row.
      * Data row numbering starts at 1 and ends at numRows(). */
-    protected void setRowName(int row, String name) { data[row][0] = name; }
+    public void setRowName(int row, String name) { data[row][0] = name; }
     /** Get the header name of a given row.
      * Data row numbering starts at 1 and ends at numRows(). */
     public String getRowName(int row) { return asString(data[row][0]); }
@@ -70,7 +77,7 @@ public class ResultSet {
     public int numCols() { return data[0].length - 1; }
     /** Change the header name of a given column.
      * Data column numbering starts at 1 and ends at numCols(). */
-    protected void setColName(int col, String name) { data[0][col] = name; }
+    public void setColName(int col, String name) { data[0][col] = name; }
     /** Get the header name of a given column.
      * Data column numbering starts at 1 and ends at numCols(). */
     public String getColName(int col) { return asString(data[0][col]); }
@@ -78,7 +85,7 @@ public class ResultSet {
     /** Store an object in the result set.
      * Data rows and columns are numbered starting with 1 and ending with
      * numRows() or numCols(), respectively. */
-    protected void setData(int row, int col, Object value) {
+    public void setData(int row, int col, Object value) {
         if (row == 0 || col == 0) throw new ArrayIndexOutOfBoundsException();
         data[row][col] = value;
     }
@@ -88,7 +95,36 @@ public class ResultSet {
     public SimpleData getData(int row, int col) {
         if (row == 0 || col == 0) throw new ArrayIndexOutOfBoundsException();
         Object o = data[row][col];
-        return (o instanceof SimpleData) ? (SimpleData) o : null;
+        if (o instanceof NumberData)
+            return new DoubleData
+                (((NumberData) o).getDouble() * multiplier[col], false);
+        else
+            return (o instanceof SimpleData) ? (SimpleData) o : null;
+    }
+    public void setFormat(int col, String format) {
+        int len = 0, p, s;
+        if (format == null || (len = format.length()) == 0) {
+            multiplier[col] = 1.0;
+            prefix[col] = suffix[col] = "";
+        }
+
+                                // find end of prefix
+        for (p=0;  p<len && !Character.isDigit(format.charAt(p));  p++);
+                                // find beginning of suffix
+        for (s=len;  s>0 && !Character.isDigit(format.charAt(s-1));  s--);
+
+        if (p > 0) prefix[col] = format.substring(0, p);
+        if (s > p) {
+            suffix[col] = format.substring(s);
+            String mult = format.substring(p, s);
+            try {
+                if (mult.indexOf('.') == -1)
+                    multiplier[col] = Integer.parseInt(mult);
+                else
+                    multiplier[col] = Double.parseDouble(mult);
+            } catch (NumberFormatException nfe) {}
+            if (multiplier[col] == 0) multiplier[col] = 1.0;
+        }
     }
 
     /** Format an object from the result set for display.
@@ -97,13 +133,25 @@ public class ResultSet {
      * header information. Null values in the ResultSet will be formatted as
      * the empty string. */
     public String format(int row, int col) {
-        Object o = data[row][col];
-        if (o == null)
-            return "";
-        else if (o instanceof SimpleData)
-            return ((SimpleData) o).format();
-        else
-            return o.toString();
+        // row and column headers are strings
+        if (row == 0 || col == 0) return data[row][col].toString();
+
+        SimpleData d = getData(row, col);
+        if (d == null) return "";
+
+        String result = d.format();
+        if (result.startsWith("ERR")) return result;
+
+        return prefix[col] + result + suffix[col];
+    }
+
+    /** Resort the result set by the data in the specified column.
+     * @throws ArrayIndexOutOfBoundsException unless 1 <= col <= numCols().
+     */
+    public void sortBy(int col, boolean descending) {
+        if (col < 1 || col > numCols())
+            throw new ArrayIndexOutOfBoundsException();
+        Arrays.sort(data, 1, numRows()+1, new RowComparator(col, descending));
     }
 
     /** Perform a query and return a result set. */
@@ -112,7 +160,7 @@ public class ResultSet {
                                 String basePrefix, PSPProperties props) {
 
         // Construct a regular expression for searching the repository.
-        StringBuffer re =  new StringBuffer("~.*/");
+        StringBuffer re =  new StringBuffer("~(.*/)?");
         if (conditions != null)
             for (int c=0;  c < conditions.length;  c++)
                 re.append("{").append(conditions[c]).append("}");
@@ -149,7 +197,10 @@ public class ResultSet {
             prefix = prefixes[p];
             // remove the name of the orderBy data element, & the preceeding /
             prefix = prefix.substring(0, prefix.length() - tailLen);
-            result.setRowName(p+1, prefix.substring(baseLen));
+            if (baseLen > prefix.length())
+                result.setRowName(p+1, "");
+            else
+                result.setRowName(p+1, prefix.substring(baseLen));
 
             // look up the data for this row.
             for (int d=0;  d < dataNames.length;  d++)
@@ -165,6 +216,12 @@ public class ResultSet {
                     } catch (MalformedValueException mve) { }
                     result.setData(p+1, d+1,
                                    value==null ?null :value.getSimpleValue());
+
+                } else if (dataNames[d].startsWith("\"")) { try {
+                    result.setData(p+1, d+1, new StringData(dataNames[d]));
+                } catch (MalformedValueException mve) {
+                    result.setData(p+1, d+1, null);
+                }
 
                 } else {
                     dataName = prefix + "/" + dataNames[d];
@@ -213,6 +270,19 @@ public class ResultSet {
                 result.setColName(i, colHeader);
         }
 
+        // set format for each column
+        String format;
+        for (i=dataNames.length;  i > 0;  i--) {
+            format = (String) queryParameters.get("f" + i);
+                                // if a format was given, set it.
+            if (format != null) result.setFormat(i, format);
+            // if no format was given, but the data name contains a "%",
+            // assume % format.
+            else if (!dataNames[i-1].startsWith("![(") &&
+                     dataNames[i-1].indexOf('%') != -1)
+                result.setFormat(i, "100%");
+        }
+
         return result;
     }
 
@@ -237,6 +307,27 @@ public class ResultSet {
         return "anonymousChart" + name.hashCode();
     }
 
+
+    private class RowComparator implements Comparator {
+
+        int colNum;
+        boolean descending;
+
+        public RowComparator(int colNum, boolean descending) {
+            this.colNum = colNum;
+            this.descending = descending;
+        }
+
+        public int compare(Object o1, Object o2) {
+            // The items being passed in will be rows of the ResultSet.
+            Object [] row1 = (Object[]) o1;
+            Object [] row2 = (Object[]) o2;
+
+            int result =
+                DataComparator.instance.compare(row1[colNum], row2[colNum]);
+            return descending ? 0-result : result;
+        }
+    }
 
 
     private class Category {
@@ -305,7 +396,8 @@ public class ResultSet {
     private static Double ZERO = new Double(0.0);
 
     public XYDataSource xyDataSource() {
-        ensureOneRow();
+        ensureOneRow();         // ensure SOME data exists.
+        sortBy(1, false);       // ensure data is sorted by X, ascending.
         return new RSXYDataSource();
     }
 
