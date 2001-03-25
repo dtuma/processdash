1b135e68cb526aa0791d0a0671b27c3712bf2a66
David Tuma <processdash@tuma-solutions.com>
2001-03-25 07:45:55 +0000

    New! export to excel via Microsoft Excel Web Query.
    git-svn-id: https://svn.code.sf.net/p/processdash/code/trunk@217 560cad82-bde2-470f-af40-e4e1f8d6548a

---
+++ pspdash/Templates/reports/excel.java
@@ -0,0 +1,49 @@
+// PSP Dashboard - Data Automation Tool for PSP-like processes
+// Copyright (C) 1999  United States Air Force
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//
+// The author(s) may be contacted at:
+// OO-ALC/TISHD
+// Attn: PSP Dashboard Group
+// 6137 Wardleigh Road
+// Hill AFB, UT 84056-5843
+//
+// E-Mail POC:  ken.raisor@hill.af.mil
+
+import java.io.IOException;
+import pspdash.data.DataRepository;
+import pspdash.data.ResultSet;
+
+public class excel extends pspdash.TinyCGIBase {
+
+    /** Write the CGI header. */
+    protected void writeHeader() {
+        out.print("Content-type: application/octet-stream\r\n\r\n");
+        out.flush();
+    }
+
+    protected void writeContents() throws IOException {
+        String queryString = (String) env.get("QUERY_STRING");
+        if (queryString == null || queryString.length() == 0)
+            queryString = "qf=export.rpt";
+
+        out.println("WEB");
+        out.println("1");
+        out.print("http://localhost:2468/0/reports/table.class?");
+        out.println(queryString);
+    }
+
+}
