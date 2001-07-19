af45443595224e8ff55c240f09f483c4f42571f7
David Tuma <processdash@tuma-solutions.com>
2001-07-19 03:27:22 +0000

    Initial checkin
    git-svn-id: https://svn.code.sf.net/p/processdash/code/trunk@681 560cad82-bde2-470f-af40-e4e1f8d6548a

---
+++ pspdash/XMLUtils.java
@@ -0,0 +1,81 @@
+// PSP Dashboard - Data Automation Tool for PSP-like processes
+// Copyright (C) 1999  United States Air Force
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//
+// The author(s) may be contacted at:
+// OO-ALC/TISHD
+// Attn: PSP Dashboard Group
+// 6137 Wardleigh Road
+// Hill AFB, UT 84056-5843
+//
+// E-Mail POC:  ken.raisor@hill.af.mil
+
+package pspdash;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.FactoryConfigurationError;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.DOMException;
+
+
+public class XMLUtils {
+
+    private static ResourcePool builderPool;
+    private static DocumentBuilderFactory factory = null;
+
+    static {
+        try {
+            factory = DocumentBuilderFactory.newInstance();
+        } catch (FactoryConfigurationError fce) {
+            System.err.println("Unable to create a DocumentBuilderFactory: " +
+                               fce);
+        }
+
+        builderPool = new ResourcePool() {
+                protected Object createNewResource() {
+                    try {
+                        return factory.newDocumentBuilder();
+                    } catch (Exception e) {
+                        return null;
+                    }
+                }
+            };
+    }
+
+    public static Document parse(java.io.InputStream in)
+        throws SAXException, IOException
+    {
+        DocumentBuilder builder = null;
+        Document result = null;
+        try {
+            builder = (DocumentBuilder) builderPool.get();
+            result = builder.parse(in);
+        } finally {
+            if (builder != null) builderPool.release(builder);
+        }
+        return result;
+    }
+
+}
