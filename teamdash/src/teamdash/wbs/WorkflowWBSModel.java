a3a8e93bbbeae1207a388b3690d5f5bfb2069130
David Tuma <processdash@tuma-solutions.com>
2002-12-22 05:22:39 +0000

    Code cleanup / commenting
    git-svn-id: file:///teamdash/trunk@113 9a68f182-09ab-e74b-aa6c-e8177106e7d0

--- teamdash/src/teamdash/wbs/WorkflowWBSModel.java
+++ teamdash/src/teamdash/wbs/WorkflowWBSModel.java
@@ -2,20 +2,15 @@ package teamdash.wbs;
 
 import org.w3c.dom.Element;
 
+/** Tweak the behavior of a WBSModel for use in editing common workflows.
+ */
 public class WorkflowWBSModel extends WBSModel {
 
-    public WorkflowWBSModel() {
-        super();
-    }
+    public WorkflowWBSModel()            { super();     }
+    public WorkflowWBSModel(String name) { super(name); }
+    public WorkflowWBSModel(Element e)   { super(e);    }
 
-    public WorkflowWBSModel(String name) {
-        super(name);
-    }
-
-    public WorkflowWBSModel(Element e) {
-        super(e);
-    }
-
+    /** Nodes at indentation level 1 are defined workflows. */
     public String filterNodeType(WBSNode node) {
         if (node.getIndentLevel() == 1)
             return "Workflow";
@@ -23,6 +18,7 @@ public class WorkflowWBSModel extends WBSModel {
             return super.filterNodeType(node);
     }
 
+    /** Workflows behave like software components for validation purposes. */
     public boolean isSoftwareComponent(String type) {
         return "Workflow".equals(type) || super.isSoftwareComponent(type);
     }
