b2ca4998181ccda650430f318e7e1f2c3191ee26
David Tuma <processdash@tuma-solutions.com>
2002-12-23 04:54:22 +0000

    Code cleanup / commenting
    git-svn-id: file:///teamdash/trunk@125 9a68f182-09ab-e74b-aa6c-e8177106e7d0

--- teamdash/src/teamdash/wbs/columns/Pruner.java
+++ teamdash/src/teamdash/wbs/columns/Pruner.java
@@ -2,14 +2,17 @@ package teamdash.wbs.columns;
 
 import teamdash.wbs.WBSNode;
 
+/** Interface used by {@link TopDownBottomUpColumn} to identify nodes
+ * which should be excluded from bottom-up calculations.
+ */
 public interface Pruner {
 
     /** Returns true if the given node should not participate in the
-         * bottom-up calculation. Nodes that are pruned from the calculation (and
-         * all the children of pruned nodes) will have neither their "top down"
-         * nor their "bottom up" attribute set.  Instead, they will have their
+     * bottom-up calculation. Nodes that are pruned from the calculation (and
+     * all the children of pruned nodes) will have neither their "top down"
+     * nor their "bottom up" attribute set.  Instead, they will have their
      * "inherited" attribute set to the value of their nearest ancestor.
-         */
+     */
     public boolean shouldPrune(WBSNode node);
 
 }
