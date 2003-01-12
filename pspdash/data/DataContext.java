6fdec8f03c7939c269abc516aae821de9dc404e2
David Tuma <processdash@tuma-solutions.com>
2003-01-12 03:22:09 +0000

    May use these someday for improved efficiency
    git-svn-id: https://svn.code.sf.net/p/processdash/code/trunk@1569 560cad82-bde2-470f-af40-e4e1f8d6548a

---
+++ pspdash/data/DataContext.java
@@ -0,0 +1,11 @@
+package pspdash.data;
+
+public interface DataContext {
+
+    public SaveableData getValue(String name);
+
+    public SimpleData getSimpleValue(String name);
+
+    public void putValue(String name, SaveableData value);
+
+}
