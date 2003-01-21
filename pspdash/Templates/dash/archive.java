d4a04e25f460328fd06b4880c3784f84de6b8605
David Tuma <processdash@tuma-solutions.com>
2003-01-21 02:03:16 +0000

    Display a "wait" page, telling the user that the archive is being created, please wait.  Allow the caller to specify a base filename for the archive;  append the current date onto the file by default.
    git-svn-id: https://svn.code.sf.net/p/processdash/code/trunk@1578 560cad82-bde2-470f-af40-e4e1f8d6548a

--- pspdash/Templates/dash/archive.java
+++ pspdash/Templates/dash/archive.java
@@ -4,12 +4,15 @@ import pspdash.TinyWebServer;
 
 import pspdash.data.FormToHTML;
 
+import java.io.InputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLDecoder;
+import java.net.URLEncoder;
 
 import java.text.SimpleDateFormat;
 
@@ -17,6 +20,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.Random;
 
 
@@ -25,33 +29,85 @@ public class archive extends TinyCGIBase {
     private String boundary;
     private HashSet seenURIs;
 
-    protected void writeHeader() {
-        out.print("Content-type: application/octet-stream" + CRLF);
-        out.print("Content-Disposition: attachment; " +
-            "filename=\"fname.mhtml\"" + CRLF + CRLF);
+    public void service(InputStream in, OutputStream out, Map env)
+        throws IOException
+    {
+        super.service(in, out, env);
 
-        // flush in case writeContents wants to use outStream instead of out.
+        if (parameters.containsKey("run")) {
+            writeArchiveHeader();
+            writeArchiveContents();
+        } else {
+            writeWaitHeader();
+            writeWaitContents();
+        }
+
+        this.out.flush();
+    }
+
+    protected void writeHeader() { }
+    protected void writeContents() { }
+
+    protected void writeWaitHeader() {
+        out.print("Content-type: text/html" + CRLF + CRLF);
         out.flush();
     }
 
 
-    protected void writeContents() throws IOException {
-        String startingURI = getURI();
+    protected void writeWaitContents() {
+        String uri = getURI();
+        String filename = getFilename(uri);
 
-        if (startingURI == null)
-            throw new NullPointerException();
-
-        // We don't actually use the correct URL to the dashboard - if we did,
-        // missing items would automatically get loaded from the running
-        // dashboard.
-        base = "http://localhost:9999";
-        boundary = createBoundary();
-        seenURIs = new HashSet();
-
-        writeMimeHeader();
-        writeItemAndRecurse(startingURI);
-        writeMimeEnding();
+        out.print("<html><head>\n");
+        out.print("<title>Archiving Dashboard Data</title>\n");
+        out.print("<meta http-equiv='Refresh' "+
+                  "content='1;URL=archive.class?run&uri=");
+        out.print(URLEncoder.encode(uri));
+        out.print("&filename=");
+        out.print(URLEncoder.encode(filename));
+        out.print("'>\n");
+        out.print("</head><body><h1>Archiving Dashboard Data</h1>\n");
+        out.print("Please wait while the dashboard creates a web archive ");
+        out.print("of your data. When the archive is ready, your download ");
+        out.print("should begin automatically.\n");
+        out.print("</body></html>\n");
     }
+
+    private String getFilename(String uri) {
+        String filename = getParameter("filename");
+        if (filename == null)
+            return "dash-archive";
+
+        if (filename.indexOf("PREFIX") != -1) {
+            String prefix = getPrefixFromURI(uri);
+            int pos = prefix.lastIndexOf('/');
+            if (pos != -1) prefix = prefix.substring(pos+1);
+            prefix = makeSafe(prefix);
+            filename = StringUtils.findAndReplace(filename, "PREFIX", prefix);
+        }
+
+        return filename;
+    }
+
+    private static String makeSafe(String s) {
+        if (s == null) s = "";
+        s = s.trim();
+        // perform a round-trip through the default platform encoding.
+        s = new String(s.getBytes());
+
+        StringBuffer result = new StringBuffer(s);
+        char c;
+        for (int i = result.length();   i-- > 0; )
+            if (-1 == ULTRA_SAFE_CHARS.indexOf(result.charAt(i)))
+                result.setCharAt(i, '_');
+
+        return result.toString();
+    }
+    private static final String ULTRA_SAFE_CHARS =
+        "abcdefghijklmnopqrstuvwxyz" +
+        "0123456789" + "_" +
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
 
 
     /** Determine the URI of the item to archive.
@@ -76,6 +132,39 @@ public class archive extends TinyCGIBase {
         }
 
         return null;
+    }
+
+
+
+    protected void writeArchiveHeader() {
+        String filename = getParameter("filename");
+        if (filename == null) filename = "dash-archive";
+        filename = filename + "-" + filenameDateFormat.format(new Date());
+
+        out.print("Content-type: application/octet-stream" + CRLF);
+        out.print("Content-Disposition: attachment; " +
+            "filename=\"" + filename + ".mhtml\"" + CRLF + CRLF);
+
+        // flush in case writeContents wants to use outStream instead of out.
+        out.flush();
+    }
+
+
+    protected void writeArchiveContents() throws IOException {
+        String startingURI = getParameter("uri");
+        if (startingURI == null)
+            throw new NullPointerException();
+
+        // We don't actually use the correct URL to the dashboard - if we did,
+        // missing items would automatically get loaded from the running
+        // dashboard.
+        base = "http://localhost:9999";
+        boundary = createBoundary();
+        seenURIs = new HashSet();
+
+        writeMimeHeader();
+        writeItemAndRecurse(startingURI);
+        writeMimeEnding();
     }
 
 
@@ -491,6 +580,10 @@ public class archive extends TinyCGIBase {
 
     private static final String CRLF = "\r\n";
     private static final SimpleDateFormat dateFormat =
-        // Tue, 05 Dec 2000 17:28:07 GMT
+        // ------------------ Tue, 05 Dec 2000 17:28:07 GMT
         new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz");
+    private static final SimpleDateFormat filenameDateFormat =
+        // ------------------ 05-Dec-2000
+        new SimpleDateFormat("dd-MMM-yyyy");
+
 }
