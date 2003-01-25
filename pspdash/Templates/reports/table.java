a1d031f7d1c455118d97d3a51355d38a9cbf38ec
David Tuma <processdash@tuma-solutions.com>
2003-01-25 06:56:54 +0000

    undo the previous change - MimeHTMLArchiver is smarter now.
    git-svn-id: https://svn.code.sf.net/p/processdash/code/trunk@1589 560cad82-bde2-470f-af40-e4e1f8d6548a

--- pspdash/Templates/reports/table.java
+++ pspdash/Templates/reports/table.java
@@ -80,10 +80,8 @@ public class table extends pspdash.TinyCGIBase {
         if (!includable) {
             out.println("</TABLE>");
             if (foot != null) out.println("<P>" + foot + "</P>");
-            if (!parameters.containsKey("EXPORT"))
-                out.print("<P class='doNotPrint'><A HREF=\"excel.iqy\">" +
-                          "<I>Export to Excel</I></A>");
-            out.println("</BODY></HTML>");
+            out.print("<P class='doNotPrint'><A HREF=\"excel.iqy\">" +
+                      "<I>Export to Excel</I></A></P></BODY></HTML>");
         }
     }
 
