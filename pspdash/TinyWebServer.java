3007c16379b2fb23d9c4496494257fc642b0b156
David Tuma <processdash@tuma-solutions.com>
2003-04-02 04:28:57 +0000

    Modification to AddOnClassLoader - call defineClass more exactly according to spec.
    git-svn-id: https://svn.code.sf.net/p/processdash/code/trunk@1690 560cad82-bde2-470f-af40-e4e1f8d6548a

--- pspdash/TinyWebServer.java
+++ pspdash/TinyWebServer.java
@@ -1,5 +1,5 @@
 // PSP Dashboard - Data Automation Tool for PSP-like processes
-// Copyright (C) 1999  United States Air Force
+// Copyright (C) 2003 Software Process Dashboard Initiative
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -21,7 +21,7 @@
 // 6137 Wardleigh Road
 // Hill AFB, UT 84056-5843
 //
-// E-Mail POC:  ken.raisor@hill.af.mil
+// E-Mail POC:  processdash-devel@lists.sourceforge.net
 
 package pspdash;
 
@@ -130,8 +130,8 @@ public class TinyWebServer extends Thread {
                 throw new ClassNotFoundException(name);
 
             try {
-                name = name.replace('.', '/');
-                URL classURL = new URL(base, name + ".class");
+                String filename = name.replace('.', '/') + ".class";
+                URL classURL = new URL(base, filename);
                 URLConnection conn = classURL.openConnection();
                 conn.connect();
 
