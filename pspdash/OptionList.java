90a1aefde5a1e8c5ea62d6f2ab110124d3d0f226
David Tuma <processdash@tuma-solutions.com>
2003-06-12 03:32:27 +0000

    upgrade support for HTML rendering.
    git-svn-id: https://svn.code.sf.net/p/processdash/code/trunk@1731 560cad82-bde2-470f-af40-e4e1f8d6548a

--- pspdash/OptionList.java
+++ pspdash/OptionList.java
@@ -1,5 +1,5 @@
 // PSP Dashboard - Data Automation Tool for PSP-like processes
-// Copyright (C) 1999  United States Air Force
+// Copyright (C) 2003 Software Process Dashboard Initiative
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -21,7 +21,7 @@
 // 6137 Wardleigh Road
 // Hill AFB, UT 84056-5843
 //
-// E-Mail POC:  ken.raisor@hill.af.mil
+// E-Mail POC:  processdash-devel@lists.sourceforge.net
 
 
 package pspdash;
@@ -65,13 +65,26 @@ public class OptionList {
         comments = Collections.unmodifiableMap(comments);
     }
 
+
+    /** Construct a new option list containing the items in the
+     * given collection.
+     */
+    public OptionList(Collection opts) {
+        options = new Vector(opts);
+        comments = Collections.EMPTY_MAP;
+    }
+
+
     public String getAsHTML(String name) {
         StringBuffer result = new StringBuffer();
-        result.append("<SELECT NAME='").append(name).append("'>\n");
+        result.append("<SELECT NAME='")
+            .append(HTMLUtils.escapeEntities(name)).append("'>\n");
         Iterator i = options.iterator();
         String option;
         while (i.hasNext())
-            result.append("<OPTION>").append((String) i.next()).append("\n");
+            result.append("<OPTION>")
+                .append(HTMLUtils.escapeEntities((String) i.next()))
+                .append("\n");
         result.append("</SELECT>");
         return result.toString();
     }
