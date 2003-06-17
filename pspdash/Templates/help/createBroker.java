0eabc05f71d3d93963c6d0193e47aaaf5a4884bc
David Tuma <processdash@tuma-solutions.com>
2003-06-17 04:07:14 +0000

    Load help through the web server
    git-svn-id: https://svn.code.sf.net/p/processdash/code/trunk@1767 560cad82-bde2-470f-af40-e4e1f8d6548a

--- pspdash/Templates/help/createBroker.java
+++ pspdash/Templates/help/createBroker.java
@@ -1,19 +1,19 @@
 // PSP Dashboard - Data Automation Tool for PSP-like processes
 // Copyright (C) 2003 Software Process Dashboard Initiative
 //
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License
-// as published by the Free Software Foundation; either version 2
-// of the License, or (at your option) any later version.
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
 //
-// This program is distributed in the hope that it will be useful,
+// This library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
 //
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+//Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 //
 // The author(s) may be contacted at:
 // OO-ALC/TISHD
@@ -33,6 +33,7 @@ import java.io.InputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.URL;
+import java.net.MalformedURLException;
 import java.util.Map;
 import javax.help.*;
 
@@ -53,7 +54,12 @@ public class createBroker extends DefaultHelpBroker
     public createBroker() throws HelpSetException {
         super();
 
-        URL hsURL = TemplateLoader.resolveURL(HELPSET_PATH);
+        URL hsURL = null;
+        try {
+            hsURL = new URL(Browser.mapURL(HELPSET_PATH));
+        } catch (MalformedURLException mue) {
+            throw new HelpSetException("Couldn't create helpset url");
+        }
 
         HelpSet hs = new HelpSet(null,hsURL);
         //System.out.println("Found help set at " + hsURL);
