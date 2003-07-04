4f8714157a75523f13aaf7ac96d19abc8b731dc2
David Tuma <processdash@tuma-solutions.com>
2003-07-04 03:10:44 +0000

    Localization
    git-svn-id: https://svn.code.sf.net/p/processdash/code/trunk@1805 560cad82-bde2-470f-af40-e4e1f8d6548a

--- pspdash/EVTask.java
+++ pspdash/EVTask.java
@@ -160,7 +160,8 @@ public class EVTask implements DataListener {
 
     private static final Date COMPLETION_DATE_NA = EVSchedule.A_LONG_TIME_AGO;
 
-    static Resources resources = Resources.getDashBundle("pspdash.EVTask");
+    static Resources resources =
+        Resources.getDashBundle("pspdash.TaskScheduleDialog");
 
     /** Creates an EVTask suitable for the root of an EVTaskList.  */
     public EVTask(String rootName) {
