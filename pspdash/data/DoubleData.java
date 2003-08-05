157e552d4bede10e5ea6180eb9e172b6be6ac1d6
David Tuma <processdash@tuma-solutions.com>
2003-08-05 03:48:31 +0000

    Bugfix - test() would not work properly for ImmutableDoubleData objects
    git-svn-id: https://svn.code.sf.net/p/processdash/code/trunk@1827 560cad82-bde2-470f-af40-e4e1f8d6548a

--- pspdash/data/DoubleData.java
+++ pspdash/data/DoubleData.java
@@ -1,5 +1,5 @@
 // PSP Dashboard - Data Automation Tool for PSP-like processes
-// Copyright (C) 1999  United States Air Force
+// Copyright (C) 2003 Software Process Dashboard Initiative
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -21,7 +21,7 @@
 // 6137 Wardleigh Road
 // Hill AFB, UT 84056-5843
 //
-// E-Mail POC:  ken.raisor@hill.af.mil
+// E-Mail POC:  processdash-devel@lists.sourceforge.net
 
 package pspdash.data;
 
@@ -170,7 +170,8 @@ public class DoubleData implements SimpleData, NumberData {
     }
     public boolean test() {
         // should NaN and Infinity count as true values?
-        return (getDouble() != 0.0 &&
+        double value = getDouble();
+        return (value != 0.0 &&
                 !Double.isNaN(value) &&
                 !Double.isInfinite(value));
     }
