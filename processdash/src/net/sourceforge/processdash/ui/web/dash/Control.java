4e184abb10052bcd06d740d004508ada2e968b31
David Tuma <processdash@tuma-solutions.com>
2003-10-28 03:18:22 +0000

    Moved from Templates/control
    git-svn-id: https://svn.code.sf.net/p/processdash/code/trunk@1993 560cad82-bde2-470f-af40-e4e1f8d6548a

---
+++ processdash/src/net/sourceforge/processdash/ui/web/dash/Control.java
@@ -0,0 +1,142 @@
+// PSP Dashboard - Data Automation Tool for PSP-like processes
+// Copyright (C) 1999  United States Air Force
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//
+// The author(s) may be contacted at:
+// OO-ALC/TISHD
+// Attn: PSP Dashboard Group
+// 6137 Wardleigh Road
+// Hill AFB, UT 84056-5843
+//
+// E-Mail POC:  ken.raisor@hill.af.mil
+
+package net.sourceforge.processdash.ui.web.dash;
+
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Date;
+
+import javax.swing.SwingUtilities;
+
+import pspdash.ConsoleWindow;
+import pspdash.DashController;
+import pspdash.TinyCGIBase;
+
+
+public class Control extends TinyCGIBase {
+
+    private String taskName;
+    private boolean printNullDocument;
+
+    /** Write the CGI header. */
+    protected void writeHeader() {
+        out.print("Expires: 0\r\n");
+        super.writeHeader();
+    }
+
+    /** Generate CGI script output. */
+    protected void writeContents() throws IOException {
+        DashController.checkIP(env.get("REMOTE_ADDR"));
+        printNullDocument = true;
+        taskName = (String) env.get("SCRIPT_NAME");
+
+        raiseWindow();
+        showConsole();
+        setPath();
+        startTiming();
+        stopTiming();
+        clearCGICache();
+
+        if (printNullDocument)
+            DashController.printNullDocument(out);
+    }
+
+    private boolean isTask(String task) {
+        return taskName.indexOf(task) != -1;
+    }
+
+    private void raiseWindow() {
+        if (isTask("raiseWindow")) DashController.raiseWindow();
+    }
+
+    private void showConsole() {
+        if (isTask("showConsole")) ConsoleWindow.showInstalledConsole();
+    }
+
+    private void startTiming() {
+        if (isTask("startTiming")) DashController.startTiming();
+    }
+
+    private void setPath() throws IOException {
+        if (isTask("setPath")) {
+            try {
+                boolean startTiming = parameters.containsKey("start");
+                String phase = getParameter("phase");
+                SwingUtilities.invokeAndWait(
+                    new SetPath(startTiming, getPrefix(), phase));
+            } catch (InterruptedException ie) {
+            } catch (InvocationTargetException ite) {
+                if (ite.getTargetException() instanceof IOException)
+                    throw (IOException) ite.getTargetException();
+            }
+        }
+    }
+
+    private void stopTiming() {
+        if (isTask("stopTiming")) DashController.stopTiming();
+    }
+
+    private void clearCGICache() {
+        if (isTask("clearCGI")) {
+            getTinyWebServer().clearClassLoaderCaches();
+
+            out.println("<HTML><HEAD><TITLE>Classes cleared</TITLE></HEAD>");
+            out.println("<BODY><H1>Classes cleared</H1>");
+            out.println("The classloader cache was cleared at " + new Date());
+            out.println("</BODY></HTML>");
+
+            printNullDocument = false;
+        }
+    }
+
+
+    private class SetPath implements Runnable {
+
+        private boolean start;
+        private String prefix;
+        private String phase;
+
+        SetPath(boolean start, String prefix, String phase) {
+            this.start = start;
+            this.prefix = prefix;
+            this.phase = phase;
+        }
+
+        public void run() {
+            if (DashController.setPath(prefix) == false)
+                start = false;
+            else if (phase != null) {
+                if (DashController.setPhase(phase) == false)
+                    start = false;
+            }
+            if (start)
+                DashController.startTiming();
+        }
+
+    }
+
+}
