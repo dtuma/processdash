0bc79df41b672e7e3283f7af202b6687ab62e91d
David Tuma <processdash@tuma-solutions.com>
2003-11-05 17:16:13 +0000

    Moved from Templates/control
    git-svn-id: https://svn.code.sf.net/p/processdash/code/trunk@2055 560cad82-bde2-470f-af40-e4e1f8d6548a

---
+++ processdash/src/net/sourceforge/processdash/tool/export/ui/DumpDataElements.java
@@ -0,0 +1,56 @@
+// Process Dashboard - Data Automation Tool for high-maturity processes
+// Copyright (C) 2003 Software Process Dashboard Initiative
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//
+// The author(s) may be contacted at:
+// Process Dashboard Group
+// c/o Ken Raisor
+// 6137 Wardleigh Road
+// Hill AFB, UT 84056-5843
+//
+// E-Mail POC:  processdash-devel@lists.sourceforge.net
+
+package net.sourceforge.processdash.tool.export.ui;
+
+
+import java.io.IOException;
+import java.util.Vector;
+
+import net.sourceforge.processdash.net.http.TinyCGIHighVolume;
+import net.sourceforge.processdash.ui.web.TinyCGIBase;
+
+import pspdash.DashController;
+
+
+public class DumpDataElements extends TinyCGIBase implements TinyCGIHighVolume {
+
+    protected void writeHeader() {
+        out.print("Content-type: text/plain\r\n\r\n");
+    }
+
+    /** Generate CGI script output. */
+    protected void writeContents() throws IOException {
+        DashController.checkIP(env.get("REMOTE_ADDR"));
+        Vector filter = null;
+        String prefix = getPrefix();
+        if (prefix != null && prefix.length() > 1) {
+            filter = new Vector();
+            filter.add(prefix);
+        }
+        getDataRepository().dumpRepository(out, filter, true);
+    }
+
+}
---
+++ processdash/src/net/sourceforge/processdash/tool/export/ui/ExportNow.java
@@ -0,0 +1,50 @@
+// Process Dashboard - Data Automation Tool for high-maturity processes
+// Copyright (C) 2003 Software Process Dashboard Initiative
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//
+// The author(s) may be contacted at:
+// Process Dashboard Group
+// c/o Ken Raisor
+// 6137 Wardleigh Road
+// Hill AFB, UT 84056-5843
+//
+// E-Mail POC:  processdash-devel@lists.sourceforge.net
+
+package net.sourceforge.processdash.tool.export.ui;
+
+
+import java.io.IOException;
+import java.util.Date;
+
+import pspdash.DashController;
+
+import net.sourceforge.processdash.ui.web.TinyCGIBase;
+
+
+
+public class ExportNow extends TinyCGIBase {
+
+    /** Generate CGI script output. */
+    protected void writeContents() throws IOException {
+        DashController.exportData(getPrefix());
+
+        out.println("<HTML><HEAD><TITLE>Data Exported</TITLE></HEAD>");
+        out.println("<BODY><H1>Data Exported</H1>");
+        out.println("The data was exported at " + new Date());
+        out.println("</BODY></HTML>");
+    }
+
+}
