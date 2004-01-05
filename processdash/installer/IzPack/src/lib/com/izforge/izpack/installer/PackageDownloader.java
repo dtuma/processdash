d8aa14449b8669853bb9179557edb5d6a14f1282
David Tuma <processdash@tuma-solutions.com>
2004-01-05 01:57:13 +0000

    New feature for dashboard: dynamic download of packages, with GUI feedback.
    git-svn-id: https://svn.code.sf.net/p/processdash/code/trunk@2193 560cad82-bde2-470f-af40-e4e1f8d6548a

---
+++ processdash/installer/IzPack/src/lib/com/izforge/izpack/installer/PackageDownloader.java
@@ -0,0 +1,111 @@
+
+package com.izforge.izpack.installer;
+
+import java.awt.*;
+import java.io.*;
+import java.net.*;
+import javax.swing.*;
+
+public class PackageDownloader extends Thread {
+
+    /** We want the user to be aware that something was downloaded.
+     * Therefore, even if the download is instantaneous, we will display
+     * the download progress dialog for at least this many milliseconds.
+     */
+    private static final long MIN_DELAY = 1000;
+
+    private static final int MIN_DELAY_OFFSET = 1000;
+
+    private String packageName;
+
+    private JDialog dialog;
+
+    private JProgressBar progressBar;
+
+    private long totalSize;
+
+    private InputStream inputStream;
+
+    public PackageDownloader(String packageName, String packageLocation)
+    {
+        this.packageName = packageName;
+
+        try {
+            URL url = new URL(packageLocation);
+            URLConnection conn = url.openConnection();
+            conn.setAllowUserInteraction(true);
+            conn.setRequestProperty
+                ("X-Process-Dashboard-Installer", "1.6");
+            totalSize = conn.getContentLength();
+            inputStream = conn.getInputStream();
+
+            buildDialog();
+            start();
+            dialog.show();
+        } catch (IOException ioe) {
+            inputStream = null;
+        }
+    }
+
+    public InputStream getInputStream() {
+        return inputStream;
+    }
+
+    private void buildDialog() {
+        dialog = new JDialog((Frame) null, "Downloading...", true);
+        dialog.getContentPane().setLayout(new GridLayout(2, 1, 10, 10));
+
+        String label = "Downloading '" + packageName + "'...";
+        dialog.getContentPane().add(new JLabel(label));
+
+        if (totalSize > 0) {
+            progressBar = new JProgressBar(-MIN_DELAY_OFFSET, (int) totalSize);
+            dialog.getContentPane().add(progressBar);
+        }
+
+        dialog.pack();
+        Dimension dialogSize = dialog.getSize();
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        dialog.setLocation((screenSize.width - dialogSize.width) / 2,
+                           (screenSize.height - dialogSize.height) / 2 - 10);
+    }
+
+    public void run() {
+        try {
+            long startTime = System.currentTimeMillis();
+
+            ByteArrayOutputStream slurpBuffer = null;
+            if (totalSize <= 0)
+                slurpBuffer = new ByteArrayOutputStream();
+            else
+                slurpBuffer = new ByteArrayOutputStream
+                    ((int) (totalSize + 100));
+
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            int totalBytesRead = 0;
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                slurpBuffer.write(buffer, 0, bytesRead);
+                totalBytesRead += bytesRead;
+                if (progressBar != null)
+                    progressBar.setValue(totalBytesRead - MIN_DELAY_OFFSET);
+            }
+
+            long endTime = System.currentTimeMillis();
+            long elapsed = endTime - startTime;
+            long remaining = MIN_DELAY - elapsed;
+            if (remaining > 0)
+                Thread.currentThread().sleep(remaining);
+            if (progressBar != null)
+                progressBar.setValue(totalBytesRead);
+
+            inputStream = new ByteArrayInputStream(slurpBuffer.toByteArray());
+        } catch (Exception e) {
+            inputStream = null;
+        } finally {
+            dialog.dispose();
+        }
+    }
+
+
+}
