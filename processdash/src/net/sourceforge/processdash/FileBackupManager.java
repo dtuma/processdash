b08c14817a7ebf52dd95e9cb3a1aa0bd78ec01c3
David Tuma <processdash@tuma-solutions.com>
2005-04-30 03:26:58 +0000

    Enhance the backup mechanism, so it facilitates data recovery for end users
    git-svn-id: https://svn.code.sf.net/p/processdash/code/trunk@2448 560cad82-bde2-470f-af40-e4e1f8d6548a

--- processdash/src/net/sourceforge/processdash/FileBackupManager.java
+++ processdash/src/net/sourceforge/processdash/FileBackupManager.java
@@ -1,5 +1,5 @@
 // Process Dashboard - Data Automation Tool for high-maturity processes
-// Copyright (C) 2003 Software Process Dashboard Initiative
+// Copyright (C) 2003,2005 Software Process Dashboard Initiative
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -27,9 +27,11 @@
 package net.sourceforge.processdash;
 
 import java.io.*;
+import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.zip.*;
 
+import net.sourceforge.processdash.net.http.WebServer;
 import net.sourceforge.processdash.ui.*;
 
 
@@ -39,7 +41,8 @@ import net.sourceforge.processdash.ui.*;
  * (time.log), the state file (state), user settings (pspdash.ini), and
  * the error log (log.txt).
  *
- * Do this each time the dashboard starts.  Also do it at midnight.
+ * Do this each time the dashboard starts or shuts down.
+ * Also do it at midnight.
  */
 public class FileBackupManager {
 
@@ -49,76 +52,294 @@ public class FileBackupManager {
 
     private static OutputStream logFile = null;
     private static final String LOG_FILE_NAME = "log.txt";
+    private static final String OLD_BACKUP_TEMP_FILENAME = "temp_old.zip";
+    private static final String NEW_BACKUP_TEMP_FILENAME = "temp_new.zip";
+    private static final SimpleDateFormat DATE_FMT =
+        new SimpleDateFormat("yyyyMMddHHmmss");
+    private static final long DAY_MILLIS = 24L /*hours*/ * 60 /*minutes*/
+        * 60 /*seconds*/ * 1000 /*millis*/;
+
 
     public static void maybeRun(String dataDirName, int when) {
-        if ("true".equalsIgnoreCase(Settings.getVal("backup.enabled"))) try {
-            run(dataDirName, when);
-        } catch (Throwable t) {}
+        if (Settings.getBool("backup.enabled", false)) {
+            try {
+                run(dataDirName, when);
+            } catch (Throwable t) {}
+        } else if (when == STARTUP &&
+                   Settings.getBool("logging.enabled", false)) {
+            startLogging(new File(dataDirName));
+        }
     }
+
 
     public synchronized static void run(String dataDirName, int when) {
         File dataDir = new File(dataDirName);
         File backupDir = new File(dataDir, "backup");
         if (!backupDir.exists()) backupDir.mkdir();
         if (!backupDir.exists()) return;
+        boolean loggingEnabled = Settings.getBool("logging.enabled", false);
 
-        File fileList = new File(backupDir, "list.txt");
-        HashMap origFileTimes = new HashMap();
-        HashMap origFileSizes = new HashMap();
-        HashMap fileTimes = new HashMap();
-        HashMap fileSizes = new HashMap();
+        if (loggingEnabled)
+            stopLogging();
 
-        stopLogging();
+        try {
+            backupFiles(dataDir, backupDir, when);
+        } catch (Exception e) {
+            printError(e);
+        }
 
-        loadFileInfo(fileList, origFileTimes, origFileSizes);
-
-        backupFiles(dataDir, backupDir,
-                    origFileTimes, origFileSizes,
-                    fileTimes, fileSizes, when);
-
-        saveFileInfo(fileList, fileTimes, fileSizes);
-
-        if (when != SHUTDOWN)
+        if (loggingEnabled && when != SHUTDOWN)
             startLogging(dataDir);
     }
 
-    private static void loadFileInfo(File list, Map fileTimes, Map fileSizes) {
-        if (!list.exists()) return;
-        try {
-            BufferedReader in = new BufferedReader(new FileReader(list));
-            String line, name, time, size;
-            while ((line = in.readLine()) != null) {
-                StringTokenizer tok = new StringTokenizer(line, DELIM);
-                name = tok.nextToken();
-                time = tok.nextToken();
-                size = tok.nextToken();
 
-                fileTimes.put(name, new Long(time));
-                fileSizes.put(name, new Long(size));
+    private static boolean oldBackupIsEmpty;
+
+
+    // Find the oldest backup in the directory.  Open it for input.
+    // Open two zip output streams: one for the new backup, and one for
+    // the old backup.
+    // Retrieve all the files in the data directory. sort. iterate:
+    //   - write the contents to the new backup zipfile.
+    //   - compare the contents to the old backup zipfile.
+    //      - If the contents are identical, do nothing.
+    //      - If the file differ (or aren't present in both places), copy
+    //        contents from the old backup input to the old backup output.
+    // Close all files.
+    // Rename the output files appropriately.
+    // Delete old/outdated backup files.
+    private static void backupFiles(File dataDir, File backupDir, int when)
+        throws IOException
+    {
+        File[] backupFiles = getBackupFiles(backupDir);
+        File oldestBackupFile = findMostRecentBackupFile(backupFiles);
+        File oldBackupTempFile = new File(backupDir, OLD_BACKUP_TEMP_FILENAME);
+        File newBackupTempFile = new File(backupDir, NEW_BACKUP_TEMP_FILENAME);
+
+        ZipInputStream oldBackupIn = null;
+        ZipOutputStream oldBackupOut = null;
+        if (oldestBackupFile != null) {
+            oldBackupIn = new ZipInputStream
+                (new FileInputStream(oldestBackupFile));
+            oldBackupOut = new ZipOutputStream
+                (new FileOutputStream(oldBackupTempFile));
+            oldBackupOut.setLevel(9);
+        }
+
+        ZipOutputStream newBackupOut = new ZipOutputStream
+            (new FileOutputStream(newBackupTempFile));
+        newBackupOut.setLevel(9);
+
+        File[] dataFiles = getDataFiles(dataDir);
+        oldBackupIsEmpty = true;
+
+        for (int i = 0; i < dataFiles.length; i++) {
+            File file = dataFiles[i];
+            ZipEntry oldEntry = positionStreams(oldBackupIn, oldBackupOut,
+                                                file);
+            if (oldEntry == null) {
+                oldBackupIn = null;
+                oldBackupOut = null;
             }
-            in.close();
-        } catch (Exception e) { printError(e); }
+            backupFile(oldEntry, oldBackupIn, oldBackupOut,
+                       newBackupOut, file);
+        }
+        // copy any additional files that might remain in oldBackupIn
+        positionStreams(oldBackupIn, oldBackupOut, null);
+
+        // finalize the new backup, and give it its final name.
+        newBackupOut.close();
+        String outputFilename = getOutputFilename(when, new Date());
+        File newBackupFile = new File(backupDir, outputFilename);
+        newBackupTempFile.renameTo(newBackupFile);
+
+        // finalize the old backup
+        if (oldBackupIn != null)
+            oldBackupIn.close();
+        if (oldestBackupFile != null) {
+            oldestBackupFile.delete();
+            if (oldBackupIsEmpty) {
+                // ZipOutputStream refuses to create an empty archive.
+                // Thus, we have to create a dummy entry to allow the
+                // subsequent close() call to succeed.
+                oldBackupOut.putNextEntry(new ZipEntry("foo"));
+                oldBackupOut.close();
+                oldBackupTempFile.delete();
+            } else {
+                if (oldBackupOut != null)
+                    oldBackupOut.close();
+                oldBackupTempFile.renameTo(oldestBackupFile);
+            }
+        }
+
+        cleanupOldBackupFiles(backupFiles);
     }
 
-    private static void saveFileInfo(File list, Map fileTimes, Map fileSizes) {
-        try {
-            Writer out = new FileWriter(list);
-            Iterator i = fileTimes.keySet().iterator();
-            String name;
-            while (i.hasNext()) {
-                name = (String) i.next();
-                out.write(name);
-                out.write(DELIM);
-                out.write(fileTimes.get(name).toString());
-                out.write(DELIM);
-                out.write(fileSizes.get(name).toString());
-                out.write("\n");
-            }
-            out.flush();
-            out.close();
-        } catch (Exception e) { printError(e); }
+
+    private static File[] getBackupFiles(File backupDir) {
+        File[] backupFiles = backupDir.listFiles(new FilenameFilter() {
+                public boolean accept(File dir, String name) {
+                    return name.startsWith("pdash-");
+                }});
+        Arrays.sort(backupFiles);
+        return backupFiles;
     }
-    private static final String DELIM = "";
+
+
+    private static File findMostRecentBackupFile(File[] backupFiles) {
+        if (backupFiles != null && backupFiles.length > 0)
+            return backupFiles[backupFiles.length - 1];
+        else
+            return null;
+    }
+
+
+    private static File[] getDataFiles(File dataDir) {
+        File[] files = dataDir.listFiles(new FileFilter() {
+                public boolean accept(File file) {
+                    return inBackupSet(file);
+                }});
+        Arrays.sort(files);
+        return files;
+    }
+
+
+    private static ZipEntry positionStreams(ZipInputStream oldBackupIn,
+                                            ZipOutputStream oldBackupOut,
+                                            File file)
+        throws IOException
+    {
+        if (oldBackupIn == null || oldBackupOut == null || file == null)
+            return null;
+
+        String fileName = null;
+        if (file != null)
+            fileName = file.getName();
+
+        while (true) {
+            ZipEntry e = oldBackupIn.getNextEntry();
+            if (e == null) {
+                oldBackupIn.close();
+                oldBackupOut.close();
+                return null;
+            }
+            if (fileName != null && fileName.equals(e.getName()))
+                return e;
+            else {
+                oldBackupIsEmpty = false;
+                copyZipEntry(oldBackupIn, oldBackupOut, e, null);
+            }
+        }
+    }
+
+
+
+    private static void copyZipEntry(InputStream oldBackupIn,
+                                     ZipOutputStream oldBackupOut,
+                                     ZipEntry e,
+                                     byte[] prepend)
+        throws IOException
+    {
+        ZipEntry eOut = new ZipEntry(e.getName());
+        eOut.setTime(e.getTime());
+        oldBackupOut.putNextEntry(eOut);
+
+        if (prepend != null)
+            oldBackupOut.write(prepend);
+
+        int bytesRead;
+        while ((bytesRead = oldBackupIn.read(copyBuf)) != -1)
+            oldBackupOut.write(copyBuf, 0, bytesRead);
+        oldBackupOut.closeEntry();
+    }
+    private static byte[] copyBuf = new byte[1024];
+
+
+    private static void backupFile(ZipEntry oldEntry,
+                                   ZipInputStream oldBackupIn,
+                                   ZipOutputStream oldBackupOut,
+                                   ZipOutputStream newBackupOut,
+                                   File file)
+        throws IOException
+    {
+        ByteArrayOutputStream bytesSeen = null;
+        BufferedInputStream oldIn = null;
+
+        // if the old backup file contains an entry for this file,
+        if (oldEntry != null && oldBackupIn != null && oldBackupOut != null) {
+            // do the prep to start comparing it with the new file.
+            bytesSeen = new ByteArrayOutputStream();
+            oldIn = new BufferedInputStream(oldBackupIn);
+        }
+
+        // create an entry in the new backup archive for this file
+        ZipEntry e = new ZipEntry(file.getName());
+        e.setTime(file.lastModified());
+        e.setSize(file.length());
+        newBackupOut.putNextEntry(e);
+
+        BufferedInputStream fileIn =
+            new BufferedInputStream(new FileInputStream(file));
+        BufferedOutputStream fileOut = new BufferedOutputStream(newBackupOut);
+        int c, d;
+        while ((c = fileIn.read()) != -1) {
+            fileOut.write(c);
+
+            // if we are still comparing the two files for identity
+            //  (they've matched so far)
+            if (oldIn != null) {
+                // read the next byte from the old backup.
+                d = oldIn.read();
+                if (d != -1)
+                    bytesSeen.write(d);
+                // if we've found a mismatch between the current file and its
+                // old backup,
+                if (c != d) {
+                    // then eagerly copy the rest of the old backup.
+                    copyZipEntry(oldIn, oldBackupOut, oldEntry,
+                                 bytesSeen.toByteArray());
+                    oldIn = null;
+                    bytesSeen = null;
+                    oldBackupIn = null;
+                    oldBackupOut = null;
+                    oldBackupIsEmpty = false;
+                }
+            }
+        }
+
+        if (oldIn != null) {
+            // read the next byte from the old backup.
+            d = oldIn.read();
+            if (d != -1) {
+                // if the old backup is longer than the current file, write it
+                // to the backup save archive.
+                bytesSeen.write(d);
+                copyZipEntry(oldIn, oldBackupOut, oldEntry, bytesSeen.
+                             toByteArray());
+                oldBackupIsEmpty = false;
+            }
+        }
+
+        // finish writing the file to the new backup archive.
+        fileOut.flush();
+        newBackupOut.closeEntry();
+    }
+
+
+    private static void cleanupOldBackupFiles(File[] backupFiles) {
+        int maxBackupAge = Settings.getInt("backup.keepBackupsNumDays", -1);
+        if (maxBackupAge > 0) {
+            long delta = maxBackupAge * DAY_MILLIS;
+            Date oldAge = new Date(System.currentTimeMillis() - delta);
+            String filename = getOutputFilename(STARTUP, oldAge);
+            for (int i = 0; i < backupFiles.length-10; i++) {
+                File file = backupFiles[i];
+                if (file.getName().compareTo(filename) < 0)
+                    file.delete();
+            }
+        }
+    }
+
 
     private static void stopLogging() {
         if (logFile != null) try {
@@ -141,46 +362,14 @@ public class FileBackupManager {
         } catch (IOException ioe) { printError(ioe); }
     }
 
-    private static void backupFiles(File dataDir, File backupDir,
-                                    Map origFileTimes, Map origFileSizes,
-                                    Map fileTimes, Map fileSizes, int when) {
 
-        long now = System.currentTimeMillis();
-        File zipFile = new File(backupDir, now + WHEN_STR[when] + ".zip");
-        FileOutputStream out = null;
-        ZipOutputStream zip = null;
-        try {
-            out = new FileOutputStream(zipFile);
-            zip = new ZipOutputStream(out);
-            zip.setLevel(9);
-        } catch (IOException ioe) {
-            printError("Error while creating backup file " + zipFile, ioe);
-        }
-
-        File[] srcFiles = dataDir.listFiles();
-        String name;
-        boolean backedSomethingUp = false;
-        for (int i = srcFiles.length;   i-- > 0; )
-            if (srcFiles[i].isFile() &&
-                inBackupSet(srcFiles[i]) &&
-                maybeBackupFile(srcFiles[i], zip,
-                                origFileTimes, origFileSizes,
-                                fileTimes, fileSizes))
-                backedSomethingUp = true;
-
-        try {
-            if (backedSomethingUp)
-                zip.close();
-            else {
-                zip = null;
-                out.close();
-                zipFile.delete();
-            }
-        } catch (Exception ioe) {
-            printError("Error while closing or deleting " + zipFile, ioe);
-        }
+    private static String getOutputFilename(int when, Date date) {
+        return "pdash-" + DATE_FMT.format(date) + WHEN_STR[when] + ".zip";
     }
-    private static final String[] WHEN_STR = { "-a", "-m", "-z" };
+
+    private static final String[] WHEN_STR = {
+        "-startup", "-checkpoint", "-shutdown"
+    };
 
     private static boolean inBackupSet(File f) {
         String name = f.getName();
@@ -196,46 +385,10 @@ public class FileBackupManager {
         return false;
     }
 
-    private static boolean maybeBackupFile(File f, ZipOutputStream zip,
-                                           Map origFileTimes,
-                                           Map origFileSizes,
-                                           Map fileTimes, Map fileSizes) {
-        String name = f.getName();
-
-        Long fileTime = new Long(f.lastModified());
-        Long fileSize = new Long(f.length());
-        fileTimes.put(name, fileTime);
-        fileSizes.put(name, fileSize);
-
-        Long prevTime = (Long) origFileTimes.get(name);
-        Long prevSize = (Long) origFileSizes.get(name);
-
-        if (prevTime != null && prevTime.equals(fileTime) &&
-            prevSize != null && prevSize.equals(fileSize))
-            return false;
-
-        try {
-            ZipEntry e = new ZipEntry(name);
-            e.setTime(fileTime.longValue());
-            zip.putNextEntry(e);
-
-            byte[] buffer = new byte[4096];
-            FileInputStream in = new FileInputStream(f);
-            int bytesRead;
-            while ((bytesRead = in.read(buffer)) != -1)
-                zip.write(buffer, 0, bytesRead);
-            in.close();
-
-        } catch (IOException ioe) {
-            printError("Error while backing up " + name, ioe);
-        }
-
-        return true;
-    }
-
     private static void printError(Throwable t) {
         printError("Unexpected error in FileBackupManager", t);
     }
+
     private static void printError(String msg, Throwable t) {
         System.err.println(msg);
         t.printStackTrace();
