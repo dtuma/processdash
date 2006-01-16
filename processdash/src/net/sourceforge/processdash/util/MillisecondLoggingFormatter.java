c6bebd8197ead7d28a4cb6f5eebad384d91ccc2d
David Tuma <processdash@tuma-solutions.com>
2006-01-16 23:18:10 +0000

    Initial checkin
    git-svn-id: https://svn.code.sf.net/p/processdash/code/trunk@2569 560cad82-bde2-470f-af40-e4e1f8d6548a

---
+++ processdash/src/net/sourceforge/processdash/util/MillisecondLoggingFormatter.java
@@ -0,0 +1,38 @@
+// Process Dashboard - Data Automation Tool for high-maturity processes
+// Copyright (C) 2006 Software Process Dashboard Initiative
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//
+// The author(s) may be contacted at:
+// Process Dashboard Group
+// c/o Ken Raisor
+// 6137 Wardleigh Road
+// Hill AFB, UT 84056-5843
+//
+// E-Mail POC:  processdash-devel@lists.sourceforge.net
+
+package net.sourceforge.processdash.util;
+
+import java.util.logging.LogRecord;
+import java.util.logging.SimpleFormatter;
+
+public class MillisecondLoggingFormatter extends SimpleFormatter {
+
+    public synchronized String format(LogRecord record) {
+        String message = super.format(record);
+        return System.currentTimeMillis() + ": " + message.substring(24);
+    }
+
+}
