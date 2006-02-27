88d9ac761e2ed1130cdc71fcccf9aa168c17fc45
David Tuma <processdash@tuma-solutions.com>
2006-02-27 02:38:04 +0000

    For lists of dependencies, add a tooltip that shows the untruncated value, with proper error highlighting indicating which values are problematic.
    git-svn-id: file:///teamdash/trunk@287 9a68f182-09ab-e74b-aa6c-e8177106e7d0

--- teamdash/src/teamdash/wbs/TaskDependencyCellRenderer.java
+++ teamdash/src/teamdash/wbs/TaskDependencyCellRenderer.java
@@ -1,8 +1,11 @@
 package teamdash.wbs;
 
 import java.awt.Component;
+import java.util.Iterator;
 
 import javax.swing.JTable;
+
+import teamdash.XMLUtils;
 
 /**
  * Table cell renderer for task dependency data.
@@ -16,19 +19,46 @@ public class TaskDependencyCellRenderer extends DataTableCellRenderer {
             boolean isSelected, boolean hasFocus, int row, int column) {
 
         boolean hasError = false;
+        setToolTipText(null);
+        String overflowToolTipText = null;
 
         // Note: this logic will not flag errors in read-only dependencies
         if (value instanceof TaskDependency)
             hasError = ((TaskDependency) value).hasError;
-        else if (value instanceof TaskDependencyList)
-            hasError = ((TaskDependencyList) value).hasError();
+        else if (value instanceof TaskDependencyList) {
+            TaskDependencyList list = (TaskDependencyList) value;
+            hasError = list.hasError();
+            overflowToolTipText = computeOverflowToolTipText(list);
+        }
 
         if (hasError)
             value = new ErrorValue(value, "Dependent task not found",
                     ErrorValue.ERROR);
 
-        return super.getTableCellRendererComponent(table, value, isSelected,
+        Component result = super.getTableCellRendererComponent(table, value, isSelected,
                 hasFocus, row, column);
+        if (overflowToolTipText != null)
+            setToolTipText(overflowToolTipText);
+        return result;
+    }
+
+    private String computeOverflowToolTipText(TaskDependencyList list) {
+        if (list.size() < 2)
+            return null;
+
+        StringBuffer result = new StringBuffer();
+        result.append("<html><body><table border='0'>");
+        for (Iterator i = list.iterator(); i.hasNext();) {
+            TaskDependency d = (TaskDependency) i.next();
+            if (d.hasError)
+                result.append("<tr><td color='red'>");
+            else
+                result.append("<tr><td>");
+            result.append(XMLUtils.escapeAttribute(d.displayName));
+            result.append("</td></tr>");
+        }
+        result.append("</table></body></html>");
+        return result.toString();
     }
 
 }
