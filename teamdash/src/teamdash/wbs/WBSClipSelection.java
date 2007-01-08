4840bc798fe0f955028b8000bffb1a7d6b7aa5a6
David Tuma <processdash@tuma-solutions.com>
2007-01-08 06:14:15 +0000

    Bugfix: previously, if a user was copying and pasting a list of nodes via text on the system clipboard (for example, because they had edited the list of tasks with MS Word or Excel), slashes in node names were let through into the WBS, where they would wreak havoc later.
    git-svn-id: file:///teamdash/trunk@410 9a68f182-09ab-e74b-aa6c-e8177106e7d0

--- teamdash/src/teamdash/wbs/WBSClipSelection.java
+++ teamdash/src/teamdash/wbs/WBSClipSelection.java
@@ -145,7 +145,7 @@ public class WBSClipSelection implements Transferable, ClipboardOwner {
 
                 WBSNode node = (WBSNode) prototype.clone();
                 node.setReadOnly(false);
-                node.setName(m.group(2));
+                node.setName(m.group(2).replace('/', ','));
                 node.setType(WBSNode.UNKNOWN_TYPE);
                 int indent = Math.max(1, node.getIndentLevel());
                 if (m.group(1) != null && m.group(1).length() > 0) {
