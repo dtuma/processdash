708c3896b03a19c92701c0c6d5946ef704c043de
David Tuma <processdash@tuma-solutions.com>
2007-10-18 05:06:25 +0000

    Bugfix: when a project was not a part of a master, the task dependency source reused the regular WBS to display a tree of tasks to the user. Unfortunately this caused the expansion/collapsed states of the two trees to be linked, resulting in cell editing errors. (Trac ticket #184)
    git-svn-id: file:///teamdash/trunk@548 9a68f182-09ab-e74b-aa6c-e8177106e7d0

--- teamdash/src/teamdash/wbs/TaskDependencySourceSimple.java
+++ teamdash/src/teamdash/wbs/TaskDependencySourceSimple.java
@@ -3,6 +3,9 @@ package teamdash.wbs;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
+
 /**
  * A source of dependency information for a standalone team project.
  * 
@@ -10,21 +13,42 @@ import java.util.Map;
  * {@link teamdash.wbs.TaskDependencySourceMaster} instead.
  * 
  */
-public class TaskDependencySourceSimple extends TaskDependencySourceAbstract {
+public class TaskDependencySourceSimple extends TaskDependencySourceAbstract
+        implements TableModelListener {
+
+    /** The WBS of the project that is using this dependency source. */
+    private WBSModel liveWbs;
+
+    /** True if we have received a change event from the liveWBS since we
+     * last refreshed our dependency tree. */
+    private boolean liveWbsHasChanged;
 
     /** For efficiency, a map of nodeID -> WBSNode for nodes in the wbs */
     private Map nodeCache;
 
     public TaskDependencySourceSimple(TeamProject project) {
-        super(project.getWBS(), project.getProjectID());
+        super(new WBSModel(), project.getProjectID());
+
+        this.liveWbs = project.getWBS();
+        this.liveWbs.addTableModelListener(this);
+        this.liveWbsHasChanged = true;
         this.nodeCache = new HashMap();
+
+        updateTaskTree();
     }
 
     public String getDisplayNameForNode(String nodeId) {
-        return getDisplayNameForNode(getTaskTree(), nodeCache, nodeId);
+        return getDisplayNameForNode(liveWbs, nodeCache, nodeId);
     }
 
     public void updateTaskTree() {
-        // nothing to do.
+        if (liveWbsHasChanged) {
+            getTaskTree().copyFrom(liveWbs);
+            liveWbsHasChanged = false;
+        }
+    }
+
+    public void tableChanged(TableModelEvent e) {
+        liveWbsHasChanged = true;
     }
 }
