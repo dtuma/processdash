c84ba85be9821d46afba3a5477f5730f8bb13e52
David Tuma <processdash@tuma-solutions.com>
2008-05-10 18:10:38 +0000

    Update copyright header comment
    git-svn-id: https://svn.code.sf.net/p/processdash/code/trunk@3369 560cad82-bde2-470f-af40-e4e1f8d6548a

--- processdash/src/net/sourceforge/processdash/ui/AboutDialog.java
+++ processdash/src/net/sourceforge/processdash/ui/AboutDialog.java
@@ -1,23 +1,27 @@
-//This class shared from the Giant Java Tree, http://www.gjt.org
-//originally in package org.gjt.fredde.util.gui;
-
-/*  AboutDialog - Displays text in a JEditorPane
- *  Copyright (C) 1999 Fredrik Ehnbom
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
+// Copyright (C) 2008 Tuma Solutions, LLC
+// Process Dashboard - Data Automation Tool for high-maturity processes
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//
+// The author(s) may be contacted at:
+// Process Dashboard Group
+// c/o Ken Raisor
+// 6137 Wardleigh Road
+// Hill AFB, UT 84056-5843
+//
+// E-Mail POC:  processdash-devel@lists.sourceforge.net
 
 package net.sourceforge.processdash.ui;
 
@@ -30,6 +34,7 @@ import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.net.URL;
 
+import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
@@ -79,8 +84,9 @@ public class AboutDialog extends JDialog implements HyperlinkListener {
         // The logo which is outside the tabbed pane
         Icon dashLogoIcon = getDashLogo();
         JLabel dashLogo = new JLabel(dashLogoIcon);
+        dashLogo.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
         dashLogo.setAlignmentX(Component.CENTER_ALIGNMENT);
-        pane.add(new JLabel(dashLogoIcon), BorderLayout.PAGE_START);
+        pane.add(dashLogo, BorderLayout.PAGE_START);
 
         // The about content
         JTabbedPane tabbedPane = new JTabbedPane();
