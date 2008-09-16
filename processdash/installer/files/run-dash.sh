6a9463d3edf31b0974cffa7084942cc784aca3c8
David Tuma <processdash@tuma-solutions.com>
2008-09-16 00:33:45 +0000

    Update the shell script comment to mention Firefox instead of Netscape.
    git-svn-id: https://svn.code.sf.net/p/processdash/code/trunk@3447 560cad82-bde2-470f-af40-e4e1f8d6548a

--- processdash/installer/files/run-dash.sh
+++ processdash/installer/files/run-dash.sh
@@ -4,9 +4,9 @@
 # Unix/Linux system.  If you aren't running on a Unix/Linux system,
 # just ignore this file.
 
-# "java" and "netscape" need to be in your path.  If they are not in
+# "java" and "firefox" need to be in your path.  If they are not in
 # your path by default, you should insert a line here to add them.
-# export PATH=$PATH:/path/to/java/dir:/path/to/netscape/dir
+# export PATH=$PATH:/path/to/java/dir:/path/to/firefox/dir
 
 # The Process Dashboard looks for data files in the current working
 # directory.  Thus, we change the cwd to point to the selected data
