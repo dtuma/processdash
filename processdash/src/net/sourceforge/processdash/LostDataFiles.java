50974d312a77cb3a5ffd32d08b0d9b7e4947da81
David Tuma <processdash@tuma-solutions.com>
2009-08-04 04:07:51 +0000

    The RobustFileOutputStream does a great job of protecting data from corruption, even when something goes terribly wrong with the filesystem. However, the dashboard was still display alarming messages to the end user. This change repairs files left behind by RobustFileOutputStream, and should be able to eliminate the alarming error messages 99.9% of the time.
    git-svn-id: https://svn.code.sf.net/p/processdash/code/trunk@3652 560cad82-bde2-470f-af40-e4e1f8d6548a

--- processdash/src/net/sourceforge/processdash/LostDataFiles.java
+++ processdash/src/net/sourceforge/processdash/LostDataFiles.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2000-2003 Tuma Solutions, LLC
+// Copyright (C) 1998-2009 Tuma Solutions, LLC
 // Process Dashboard - Data Automation Tool for high-maturity processes
 //
 // This program is free software; you can redistribute it and/or
@@ -26,11 +26,12 @@ package net.sourceforge.processdash;
 
 import java.io.File;
 import java.io.FilenameFilter;
-import java.lang.reflect.Array;
 
 import javax.swing.JOptionPane;
 
 import net.sourceforge.processdash.i18n.Resources;
+import net.sourceforge.processdash.util.FileUtils;
+import net.sourceforge.processdash.util.RobustFileOutputStream;
 
 
 // class LostDataFiles contains information and methods that are used to
@@ -38,12 +39,18 @@ import net.sourceforge.processdash.i18n.Resources;
 
 public class LostDataFiles implements FilenameFilter {
 
-    private static String acceptFilter = "tttt"; // used to determine if a file
-                                                 // is lost
-    private static String rejectFilter[] = {"tasks", "time.log"};
-                                     // an exception to acceptFilter
+    /**
+     * prefix used by {@link RobustFileOutputStream} to mark a temp file
+     */
+    private static final String TEMP_FILE_PREFIX = RobustFileOutputStream.OUT_PREFIX;
 
-    private String lostFiles[]; // contains the list of lost data files
+    /**
+     * prefix used by {@link RobustFileOutputStream} to mark a backup file
+     */
+    private static final String BACKUP_FILE_PREFIX = RobustFileOutputStream.BACKUP_PREFIX;
+
+
+    private File[] lostFiles; // contains the list of lost data files
 
     // the constructor
     public LostDataFiles() {
@@ -51,91 +58,113 @@ public class LostDataFiles implements FilenameFilter {
     }
 
     // implements the accept method for the FilenameFilter used by
-    // File.list().  Currently a file is accepted if it is not a directory
-    // starts with the letters in acceptFilter, and is not one of the
-    // strings in rejectFilter.
-
+    // File.listFiles().  A file matches if it looks like a temporary file
+    // left around by the RobustFileOutputStream logic.
     public boolean accept(File location, String filename) {
-
-        if (filename.startsWith(acceptFilter)) {
-            for(int j = 0; j < rejectFilter.length; j++) {
-                if (filename.equals(rejectFilter[j])) {
-                    return false;
-                }  // end if
-            } // end for
-
-            // Zero length files are harmless - just delete them, rather
-            // than panicking the user.
-            File f = new File(location, filename);
-            if (f.length() == 0 && f.delete())
-                return false;
-        } // end if
-        else {
-            return false;
-        }
-    return true;
+        return filename.startsWith(TEMP_FILE_PREFIX);
     }
 
-    // findLostFiles gets a list of files in the searchDir that match the
-    // acceptFilter, removes any files that match the rejectFilter, and
-    // places the result in lostFiles.
-
-    public void findLostFiles (String searchDir) {
+    /**
+     * Make a list of files that appear to be lost or corrupt.
+     */
+    public void findLostFiles(String searchDir) {
         File searchFile = new File(searchDir);
 
         // First make sure we have a directory, then get a directory list
         // that matches the accept method
         if (searchFile.isDirectory()) {
-            lostFiles = searchFile.list(this);
+            lostFiles = searchFile.listFiles(this);
         }
     }
 
-    // For now, resolve just pops up an information dialog to indicate to
-    // the user that lost data must be resolved manually.  A future change
-    // may actually allow the user to resolve the problem on the fly.
-    // true is returned if the repair was "successful".
+    /**
+     * Attempt to repair problems found, if any exist.
+     * 
+     * If no problems were found or if the repair is successful, returns true.
+     * 
+     * If the repair is unsuccessful, displays a warning dialog to the user
+     * about the problem, and asks how they wish to proceed. If the user decides
+     * to ignore the problem, returns true. Otherwise, returns false.
+     */
     public boolean repair(ProcessDashboard dash) {
-        int response;
+        if (lostFiles == null || lostFiles.length == 0)
+            return true;
 
-        ProcessDashboard parent = dash;
-
-        int lostCount = 0;
-            if (lostFiles != null) {
-                    lostCount = Array.getLength(lostFiles);
-            }
-
-        // If there are lost files, resolve them
-        if (lostCount > 0) {
-            ProcessDashboard.dropSplashScreen();
-
-            // Create an instance of the InfoDialog
-            Resources r = Resources.getDashBundle("ProcessDashboard.Errors");
-            response = JOptionPane.showConfirmDialog
-                (parent,
-                 this.printOut() + "\n" + r.getString("Lost_Data_Message"),
-                 r.getString("Lost_Data_Title"),
-                 JOptionPane.YES_NO_OPTION,
-                 JOptionPane.ERROR_MESSAGE);
-
-
-            if ((response == JOptionPane.NO_OPTION)||
-                (response == JOptionPane.CLOSED_OPTION)) {
-                return (false);
-            }
+        int unrepairedCount = 0;
+        for (int i = 0; i < lostFiles.length; i++) {
+            File file = lostFiles[i];
+            if (repairFile(file))
+                lostFiles[i] = null;
+            else
+                unrepairedCount++;
         }
-        return (true);
+        if (unrepairedCount == 0)
+            return true;
+
+        // If there are unrepaired files, display an error dialog
+        ProcessDashboard.dropSplashScreen();
+        Resources r = Resources.getDashBundle("ProcessDashboard.Errors");
+        String message = printOut() + "\n" + r.getString("Lost_Data_Message");
+        String title = r.getString("Lost_Data_Title");
+        int response = JOptionPane.showConfirmDialog(dash, message, title,
+            JOptionPane.YES_NO_OPTION, JOptionPane.ERROR_MESSAGE);
+
+        return response == JOptionPane.YES_OPTION;
+    }
+
+    private boolean repairFile(File f) {
+        if (f.getName().startsWith(BACKUP_FILE_PREFIX))
+            return fixBackupFile(f);
+        else
+            return fixTempFile(f);
+    }
+
+    private boolean fixBackupFile(File backupFile) {
+        File origFile = getFileWithoutPrefix(backupFile, BACKUP_FILE_PREFIX);
+        if (origFile == null)
+            return false;  // shouldn't happen
+
+        if (origFile.exists()) {
+            backupFile.delete();
+            return true;
+        }
+
+        try {
+            FileUtils.renameFile(backupFile, origFile);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private boolean fixTempFile(File tempFile) {
+        File origFile = getFileWithoutPrefix(tempFile, TEMP_FILE_PREFIX);
+        if (origFile == null)
+            return false;  // shouldn't happen
+
+        tempFile.delete();
+        return true;
+    }
+
+    private File getFileWithoutPrefix(File file, String prefix) {
+        if (file == null)
+            return null;
+        else if (file.getName().startsWith(prefix))
+            return new File(file.getParent(), file.getName().substring(
+                prefix.length()));
+        else
+            return null;
     }
 
     // printOut converts the data in lostFiles into a single printable string
     public String printOut() {
-        String result = "";
+        StringBuilder result = new StringBuilder();
 
-        int lostCount = Array.getLength(lostFiles);
+        for (File file : lostFiles)
+            if (file != null)
+                result.append(file.getName()).append("\n");
 
-        // If there are lost files, add them to the result
-        for (int i = 0; i < lostCount; i++) {
-            result = result + lostFiles[i] + "\n";
-        }
-    return (result);
+        return result.toString();
     }
+
 }
