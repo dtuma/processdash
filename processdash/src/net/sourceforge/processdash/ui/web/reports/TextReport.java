38aa02205990dce9d92cf6e278125abd218fe3ab
David Tuma <processdash@tuma-solutions.com>
2011-09-03 23:30:33 +0000

    Allow tabular data to be exported to XML.
    git-svn-id: https://svn.code.sf.net/p/processdash/code/trunk@4055 560cad82-bde2-470f-af40-e4e1f8d6548a

--- processdash/src/net/sourceforge/processdash/ui/web/reports/TextReport.java
+++ processdash/src/net/sourceforge/processdash/ui/web/reports/TextReport.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2001-2003 Tuma Solutions, LLC
+// Copyright (C) 2001-2011 Tuma Solutions, LLC
 // Process Dashboard - Data Automation Tool for high-maturity processes
 //
 // This program is free software; you can redistribute it and/or
@@ -26,11 +26,15 @@ package net.sourceforge.processdash.ui.web.reports;
 
 import java.io.IOException;
 
+import org.xmlpull.v1.XmlSerializer;
+
+import net.sourceforge.processdash.data.SimpleData;
 import net.sourceforge.processdash.data.util.ResultSet;
 import net.sourceforge.processdash.i18n.Resources;
 import net.sourceforge.processdash.i18n.Translator;
 import net.sourceforge.processdash.ui.web.TinyCGIBase;
 import net.sourceforge.processdash.util.HTMLUtils;
+import net.sourceforge.processdash.util.XMLUtils;
 
 
 
@@ -38,8 +42,103 @@ public class TextReport extends TinyCGIBase {
 
     static Resources resources = Resources.getGlobalBundle();
 
+    @Override
+    protected void writeHeader() {}
 
+    @Override
     protected void writeContents() throws IOException {
+        ResultSet resultSet = getResultSet();
+
+        String format = getParameter(FORMAT_PARAM);
+        if (FORMAT_XML.equals(format))
+            writeXml(resultSet);
+        else
+            writeHtml(resultSet); // default
+    }
+
+    private ResultSet getResultSet() {
+        // get the data
+        if (parameters.get("h0") == null)
+            parameters.put("h0", "Project/Task");
+        ResultSet tableData = ResultSet.get(getDataRepository(), parameters,
+            getPrefix(), getPSPProperties());
+        if (parameters.get("transpose") != null)
+            tableData = tableData.transpose();
+
+        return tableData;
+    }
+
+
+
+    /** Write result set data in XML format */
+    private void writeXml(ResultSet resultSet) throws IOException {
+        out.write("Content-Type: text/xml\r\n\r\n");
+        out.flush();
+
+        boolean whitespace = parameters.containsKey("whitespace");
+        boolean inlineAttributes = parameters.containsKey("dataAsAttr");
+
+        if (inlineAttributes) {
+            for (int col = 1; col <= resultSet.numCols(); col++) {
+                String colName = resultSet.getColName(col);
+                String attrName = textToSafeAttrName(colName);
+                resultSet.setColName(col, attrName);
+            }
+        }
+
+        XmlSerializer xml = XMLUtils.getXmlSerializer(whitespace);
+        xml.setOutput(outStream, ENCODING);
+        xml.startDocument(ENCODING, null);
+
+        xml.startTag(null, RESULT_SET_TAG);
+
+        for (int row = 1; row <= resultSet.numRows(); row++) {
+            xml.startTag(null, RESULT_ITEM_TAG);
+            xml.attribute(null, PATH_ATTR, resultSet.getRowName(row));
+
+            for (int col = 1; col <= resultSet.numCols(); col++) {
+                String name = resultSet.getColName(col);
+                SimpleData d = resultSet.getData(row, col);
+                String value = (d == null ? null : d.format());
+
+                if (inlineAttributes) {
+                    if (value != null)
+                        xml.attribute(null, name, value);
+                } else {
+                    xml.startTag(null, RESULT_DATA_TAG);
+                    xml.attribute(null, NAME_ATTR, name);
+                    if (value != null)
+                        xml.attribute(null, VALUE_ATTR, value);
+                    xml.endTag(null, RESULT_DATA_TAG);
+                }
+            }
+
+            xml.endTag(null, RESULT_ITEM_TAG);
+        }
+
+        xml.endTag(null, RESULT_SET_TAG);
+
+        xml.endDocument();
+    }
+
+    private String textToSafeAttrName(String text) {
+        StringBuffer result = new StringBuffer();
+        for (int i = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+            if (Character.isLetter(c))
+                result.append(c);
+            else
+                result.append("_");
+        }
+        return result.toString();
+    }
+
+
+
+    /** Write result set data in HTML format */
+    private void writeHtml(ResultSet tableData) throws IOException {
+        super.writeHeader();
+
         String title = tr((String) parameters.get("title"));
         String head  = tr((String) parameters.get("headerComment"));
         String foot  = tr((String) parameters.get("footerComment"));
@@ -59,14 +158,6 @@ public class TextReport extends TinyCGIBase {
             out.println(parameters.containsKey("style") ?
                         "<TABLE>" : "<TABLE BORDER>");
         }
-
-        // get the data
-        if (parameters.get("h0") == null) parameters.put("h0", "Project/Task");
-        ResultSet tableData =
-            ResultSet.get(getDataRepository(), parameters, getPrefix(),
-                          getPSPProperties());
-        if (parameters.get("transpose") != null)
-            tableData = tableData.transpose();
 
         // print the table
         int firstRow = (skipColHdr ? 1 : 0);
@@ -104,5 +195,16 @@ public class TextReport extends TinyCGIBase {
     protected String esc(String s) {
         return HTMLUtils.escapeEntities(s);
     }
+
+    private static final String FORMAT_PARAM = "format";
+    private static final String FORMAT_XML = "xml";
+
+    private static final String ENCODING = "UTF-8";
+    private static final String RESULT_SET_TAG = "pdashResultSet";
+    private static final String RESULT_ITEM_TAG = "resultItem";
+    private static final String PATH_ATTR = "path";
+    private static final String RESULT_DATA_TAG = "resultData";
+    private static final String NAME_ATTR = "name";
+    private static final String VALUE_ATTR = "value";
 
 }
