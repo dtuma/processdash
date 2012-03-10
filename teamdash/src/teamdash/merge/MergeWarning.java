d61ebc77806ff30e11e30a27efb5b840fbf7001c
David Tuma <processdash@tuma-solutions.com>
2012-03-10 22:52:00 +0000

    Initial checkin of abstract logic that can perform a 3-way merge of edits to hierarchical data.
    git-svn-id: file:///teamdash/trunk@1482 9a68f182-09ab-e74b-aa6c-e8177106e7d0

---
+++ teamdash/src/teamdash/merge/MergeWarning.java
@@ -0,0 +1,101 @@
+// Copyright (C) 2012 Tuma Solutions, LLC
+// Team Functionality Add-ons for the Process Dashboard
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 3
+// of the License, or (at your option) any later version.
+//
+// Additional permissions also apply; see the README-license.txt
+// file in the project root directory for more information.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, see <http://www.gnu.org/licenses/>.
+//
+// The author(s) may be contacted at:
+//     processdash@tuma-solutions.com
+//     processdash-devel@lists.sourceforge.net
+
+package teamdash.merge;
+
+
+public class MergeWarning<ID, Content> {
+
+    public enum Severity {
+        INFO, CONFLICT
+    };
+
+    private Severity severity;
+
+    private String key;
+
+    private ID mainNodeID;
+
+    private ID incomingNodeID;
+
+    public MergeWarning(Severity severity, String key, ID mainNodeID,
+            ID incomingNodeID) {
+        this.severity = severity;
+        this.key = key;
+        this.mainNodeID = mainNodeID;
+        this.incomingNodeID = incomingNodeID;
+    }
+
+    public MergeWarning(Severity severity, ID mainNodeID,
+            Object mainChangeType, ID incomingNodeID, Object incomingChangeType) {
+        this(severity, mainChangeType.toString() + "_"
+                + incomingChangeType.toString(), mainNodeID, incomingNodeID);
+    }
+
+    public MergeWarning(Severity severity, ID targetNodeID,
+            Object mainChangeType, Object incomingChangeType) {
+        this(severity, targetNodeID, mainChangeType, targetNodeID,
+                incomingChangeType);
+    }
+
+
+    public Severity getSeverity() {
+        return severity;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public ID getMainNodeID() {
+        return mainNodeID;
+    }
+
+    public ID getIncomingNodeID() {
+        return incomingNodeID;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null)
+            return false;
+        if (obj.getClass() != getClass())
+            return false;
+
+        MergeWarning that = (MergeWarning) obj;
+        return this.severity.equals(that.severity)
+                && this.key.equals(that.key)
+                && this.mainNodeID.equals(that.mainNodeID)
+                && this.incomingNodeID.equals(that.incomingNodeID);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = severity.hashCode();
+        result = (result << 3) ^ key.hashCode();
+        result = (result << 3) ^ mainNodeID.hashCode();
+        result = (result << 3) ^ incomingNodeID.hashCode();
+        return result;
+    }
+
+}
