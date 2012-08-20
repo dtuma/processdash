8cdb795bc5de4757aacb61c5de231ad2b436f0a0
David Tuma <processdash@tuma-solutions.com>
2012-08-20 23:05:10 +0000

    Bugfix to support drag-and-drop of files onto the Quick Launcher in a Linux/GNOME working environment.
    git-svn-id: https://svn.code.sf.net/p/processdash/code/trunk@4204 560cad82-bde2-470f-af40-e4e1f8d6548a

--- processdash/src/net/sourceforge/processdash/tool/quicklauncher/DropTransferHandler.java
+++ processdash/src/net/sourceforge/processdash/tool/quicklauncher/DropTransferHandler.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2006 Tuma Solutions, LLC
+// Copyright (C) 2006-2012 Tuma Solutions, LLC
 // Process Dashboard - Data Automation Tool for high-maturity processes
 //
 // This program is free software; you can redistribute it and/or
@@ -33,6 +33,7 @@ import java.util.Set;
 import javax.swing.JComponent;
 import javax.swing.TransferHandler;
 
+import net.sourceforge.processdash.ui.lib.TransferHandlerUtils;
 import net.sourceforge.processdash.util.LightweightSet;
 
 public class DropTransferHandler extends TransferHandler {
@@ -48,18 +49,11 @@ public class DropTransferHandler extends TransferHandler {
     }
 
     public boolean canImport(JComponent comp, DataFlavor[] flavors) {
-        for (int i = 0; i < flavors.length; i++) {
-            if (DataFlavor.javaFileListFlavor.equals(flavors[i]))
-                return true;
-        }
-        return false;
+        return TransferHandlerUtils.hasFileListFlavor(flavors);
     }
 
     public boolean importData(JComponent comp, Transferable t) {
-        List files = null;
-        try {
-            files = (List) t.getTransferData(DataFlavor.javaFileListFlavor);
-        } catch (Exception ex) {}
+        List files = TransferHandlerUtils.getTransferredFileList(t);
         if (files == null || files.isEmpty())
             return false;
 
