97af190d9ee47b3137092fb77ef6abff18bfb45e
David Tuma <processdash@tuma-solutions.com>
2012-09-13 02:28:43 +0000

    On the Size-Per-Item management page, provide a link that people can click to see a list of actual size data for past "Parts Additions"
    git-svn-id: https://svn.code.sf.net/p/processdash/code/trunk@4222 560cad82-bde2-470f-af40-e4e1f8d6548a

--- processdash/src/net/sourceforge/processdash/tool/probe/EditSizePerItemTables.java
+++ processdash/src/net/sourceforge/processdash/tool/probe/EditSizePerItemTables.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2009-2011 Tuma Solutions, LLC
+// Copyright (C) 2009-2012 Tuma Solutions, LLC
 // Process Dashboard - Data Automation Tool for high-maturity processes
 //
 // This program is free software; you can redistribute it and/or
@@ -187,9 +187,14 @@ public class EditSizePerItemTables extends TinyCGIBase {
 
                 out.println("</tr>");
             }
+            out.print("</table></li>");
         }
 
-        out.print("</table></li></ul></body></html>");
+        out.print("<li><a href='/reports/sizeEstParts.shtm'>");
+        out.print(resources.getHTML("View_Hist_Data_Option"));
+        out.println("</a></li>");
+
+        out.print("</ul></body></html>");
     }
 
 
