d6179c221756f61bffb90fa88835752338cc5f03
David Tuma <processdash@tuma-solutions.com>
2013-06-22 19:42:56 +0000

    Redact the Project_Component_Info data element when appropriate.
    git-svn-id: https://svn.code.sf.net/p/processdash/code/trunk@4274 560cad82-bde2-470f-af40-e4e1f8d6548a

--- processdash/src/net/sourceforge/processdash/tool/redact/filter/FilterDataContainingTaskNames.java
+++ processdash/src/net/sourceforge/processdash/tool/redact/filter/FilterDataContainingTaskNames.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2012 Tuma Solutions, LLC
+// Copyright (C) 2012-2013 Tuma Solutions, LLC
 // Process Dashboard - Data Automation Tool for high-maturity processes
 //
 // This program is free software; you can redistribute it and/or
@@ -103,6 +103,18 @@ public class FilterDataContainingTaskNames extends AbstractDataStringFilter {
             return prefix;
         else
             return pathNameMapper.getString(prefix);
+    }
+
+    @EnabledFor("Project_Component_Info")
+    public String hashTeamProjectComponentInfo(String xml) {
+        String[] tags = xml.split("<");
+        for (int i = 0; i < tags.length; i++) {
+            String tag = tags[i];
+            tag = RedactFilterUtils.replaceXmlAttr(tag, "name", pathNameMapper);
+            tags[i] = tag;
+        }
+
+        return StringUtils.join(Arrays.asList(tags), "<");
     }
 
 }
