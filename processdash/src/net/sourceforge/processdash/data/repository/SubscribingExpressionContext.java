502ae64c83f0edaf0edf238ed16a3171558b5787
David Tuma <processdash@tuma-solutions.com>
2013-07-01 17:00:01 +0000

    Create a data repository function that can look up the database key for a list of projects, given their alphanumeric IDs.
    git-svn-id: https://svn.code.sf.net/p/processdash/code/trunk@4288 560cad82-bde2-470f-af40-e4e1f8d6548a

--- processdash/src/net/sourceforge/processdash/data/repository/SubscribingExpressionContext.java
+++ processdash/src/net/sourceforge/processdash/data/repository/SubscribingExpressionContext.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2006 Tuma Solutions, LLC
+// Copyright (C) 2006-2013 Tuma Solutions, LLC
 // Process Dashboard - Data Automation Tool for high-maturity processes
 //
 // This program is free software; you can redistribute it and/or
@@ -36,6 +36,8 @@ import net.sourceforge.processdash.util.LightweightSet;
  */
 public class SubscribingExpressionContext implements ExpressionContext {
 
+    public static final String LISTENERVAR_NAME = ".";
+
     private DataRepository data;
 
     private String prefix;
@@ -61,6 +63,8 @@ public class SubscribingExpressionContext implements ExpressionContext {
     public SimpleData get(String dataName) {
         if (PREFIXVAR_NAME.equals(dataName))
             return StringData.create(prefix);
+        else if (LISTENERVAR_NAME.equals(dataName))
+            return StringData.create(listenerName);
 
         dataName = resolveName(dataName);
 
