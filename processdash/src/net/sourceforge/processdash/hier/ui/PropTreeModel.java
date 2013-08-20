6f1decd3a4b4b0fd7f3184eab35549e99e5a3b76
David Tuma <processdash@tuma-solutions.com>
2013-08-20 23:59:35 +0000

    Bugfix: send a notification event when changing the node of an item in a PropTreeModel.
    git-svn-id: https://svn.code.sf.net/p/processdash/code/trunk@4341 560cad82-bde2-470f-af40-e4e1f8d6548a

--- processdash/src/net/sourceforge/processdash/hier/ui/PropTreeModel.java
+++ processdash/src/net/sourceforge/processdash/hier/ui/PropTreeModel.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2000-2007 Tuma Solutions, LLC
+// Copyright (C) 2000-2013 Tuma Solutions, LLC
 // Process Dashboard - Data Automation Tool for high-maturity processes
 //
 // This program is free software; you can redistribute it and/or
@@ -89,7 +89,10 @@ public class PropTreeModel extends DefaultTreeModel
         int childIndex = 0;
         //set the current node name
         Prop value = props.pget (key);
-        node.setUserObject(key.name());
+        if (!key.name().equals(node.getUserObject())) {
+            node.setUserObject(key.name());
+            nodeChanged(node);
+        }
         int numPropChildren = value.getNumChildren();
         while (numPropChildren > childIndex) {
             if (getChildCount (node) <= childIndex) {
