df4774ecd1e173c970ff20277041da455aeb3122
David Tuma <processdash@tuma-solutions.com>
2015-02-27 21:54:57 +0000

    Deleting obsolete pspdash branch
    git-svn-id: https://svn.code.sf.net/p/processdash/code/trunk@4664 560cad82-bde2-470f-af40-e4e1f8d6548a

--- pspdash/data/js/FormDataListener.java
+++
@@ -1,7 +0,0 @@
-
-package pspdash.data.js;
-public interface FormDataListener {
-
-    public void paintData(String id, String value, boolean readOnly);
-
-}
--- pspdash/prun.bat
+++
@@ -1,1 +0,0 @@
-C:\jdk1.2.2\bin\java.exe -cp c:\src\PSPDash\PerlTools.jar;c:\src\PSPDash pspdash.PSPDashboard
