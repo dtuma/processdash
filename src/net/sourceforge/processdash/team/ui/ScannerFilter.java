69efecf5076af096d8b112a81488e5c901d4964a
David Tuma <processdash@tuma-solutions.com>
2015-07-30 19:51:23 -0600

    Create a skeleton for the Project Data Scanner tool

---
+++ src/net/sourceforge/processdash/team/ui/ScannerFilter.java
@@ -0,0 +1,41 @@
+// Copyright (C) 2015 Tuma Solutions, LLC
+// Team Functionality Add-ons for the Process Dashboard
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 3
+// of the License, or (at your option) any later version.
+//
+// Additional permissions also apply; see the README-license.txt
+// file in the project root directory for more information.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, see <http://www.gnu.org/licenses/>.
+//
+// The author(s) may be contacted at:
+//     processdash@tuma-solutions.com
+//     processdash-devel@lists.sourceforge.net
+
+package net.sourceforge.processdash.team.ui;
+
+import net.sourceforge.processdash.net.cms.PageContentTO;
+import net.sourceforge.processdash.net.cms.SnippetInstanceTO;
+import net.sourceforge.processdash.ui.snippet.SnippetPageFilter;
+
+
+/**
+ * Class to dynamically augment CMS content in the team project data scanner.
+ */
+public class ScannerFilter implements SnippetPageFilter {
+
+    public void filter(PageContentTO page, SnippetInstanceTO snippet,
+            String mode) {
+        // TODO
+    }
+
+}
