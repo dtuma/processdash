236dfbc2126f481493615ee9f4cbd98eb335cc98
David Tuma <processdash@tuma-solutions.com>
2015-12-02 10:53:51 -0500

    Redact the names of proxy estimation tables

---
+++ src/net/sourceforge/processdash/tool/redact/filter/FilterWbsProxies.java
@@ -0,0 +1,46 @@
+// Copyright (C) 2012-2015 Tuma Solutions, LLC
+// Process Dashboard - Data Automation Tool for high-maturity processes
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 3
+// of the License, or (at your option) any later version.
+//
+// Additional permissions also apply; see the README-license.txt
+// file in the project root directory for more information.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, see <http://www.gnu.org/licenses/>.
+//
+// The author(s) may be contacted at:
+//     processdash@tuma-solutions.com
+//     processdash-devel@lists.sourceforge.net
+
+package net.sourceforge.processdash.tool.redact.filter;
+
+import net.sourceforge.processdash.tool.redact.RedactFilterIDs;
+import net.sourceforge.processdash.tool.redact.EnabledFor;
+import net.sourceforge.processdash.tool.redact.HierarchyNameMapper;
+
+@EnabledFor(RedactFilterIDs.WORKFLOWS)
+public class FilterWbsProxies extends AbstractLineBasedFilter {
+
+    private HierarchyNameMapper nameMapper;
+
+    public FilterWbsProxies() {
+        setFilenamePatterns("/proxies.xml$");
+    }
+
+    public String getString(String line) {
+        if (getIndentLevel(line) == 1)
+            line = replaceXmlAttr(line, "name", nameMapper);
+
+        return line;
+    }
+
+}
