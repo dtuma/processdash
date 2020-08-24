c8f3dd8bad8427dcf74f0e8c586eb293d2b7ae31
David Tuma <processdash@tuma-solutions.com>
2020-08-23 20:37:52 -0600

    When >1 TeamTools JAR file is present, find the best one for a project
    A future version of the dashboard will ship more than one JAR file
    version of the WBS Editor code.  This provides a method for selecting
    the JAR that should be used for a given project.

--- src/net/sourceforge/processdash/tool/quicklauncher/TeamToolsVersionManager.java
+++ src/net/sourceforge/processdash/tool/quicklauncher/TeamToolsVersionManager.java
@@ -23,6 +23,8 @@
 
 package net.sourceforge.processdash.tool.quicklauncher;
 
+import static net.sourceforge.processdash.util.VersionUtils.compareVersions;
+
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -32,14 +34,23 @@ import java.io.InputStream;
 import java.util.Collections;
 import java.util.Map;
 import java.util.Properties;
+import java.util.jar.Attributes;
+import java.util.jar.JarFile;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
+
+import net.sourceforge.processdash.util.RuntimeUtils;
+
 
 public class TeamToolsVersionManager {
 
     public static final String WBS_EDITOR_VERSION_REQUIREMENT = "minWbsEditorVersion";
 
+    public static final String DATA_VERSION = "wbsDataVersion";
+
     private static final String PROJ_SETTINGS_FILENAME = "user-settings.ini";
+
+    private static final String DATA_REQ_MANIFEST_ATTR = "Dash-WBS-Min-Data-Version";
 
 
     /**
@@ -105,6 +116,113 @@ public class TeamToolsVersionManager {
         result.load(new BufferedInputStream(in));
         in.close();
         return result;
+    }
+
+
+
+    /**
+     * Look through various WBS Editor JAR files that might be packaged along
+     * with the current application, and choose the JAR that is best for opening
+     * a particular project WBS.
+     * 
+     * Note: if the given project requires a minimum version of the WBS Editor
+     * logic, and that version isn't installed, this method could return a JAR
+     * that is incapable of displaying the project. But the returned JAR will at
+     * least be capable of detecting the mismatch and displaying an error to the
+     * user.
+     * 
+     * @param wbsFile
+     *            a directory or a ZIP file containing WBS data for a project
+     * @param jarDir
+     *            the directory to search for WBS Editor JAR files, or null to
+     *            search in the directory this class is hosted from
+     * @return a JAR file containing WBS Editor logic that should be used to
+     *         open the given project. Returns null if no file could be found.
+     */
+    public static File getBestTeamToolsJarFor(File wbsFile, File jarDir) {
+        // retrieve the data version number from the WBS zip file
+        String dataVersion = getProjectProperty(wbsFile, DATA_VERSION);
+        if (dataVersion == null)
+            dataVersion = "1";
+
+        // find the directory where TeamTools JAR files should be found
+        File executableJarDirectory = jarDir;
+        if (executableJarDirectory == null)
+            executableJarDirectory = getExecutableJarDirectory();
+        if (executableJarDirectory == null)
+            return null;
+        File[] files = executableJarDirectory.listFiles();
+        if (files == null)
+            return null;
+
+        // scan the files we found, looking for the best TeamTools.jar file
+        File bestJarFile = null;
+        String bestVersion = "0";
+        for (File file : files) {
+            String version = checkAcceptableTeamToolsJar(file, dataVersion);
+            if (version != null && compareVersions(version, bestVersion) > 0) {
+                bestJarFile = file;
+                bestVersion = version;
+            }
+        }
+
+        // return the team tools JAR file we found
+        return bestJarFile;
+    }
+
+    private static File getExecutableJarDirectory() {
+        // find the classpath location of the current class
+        File selfClasspath = RuntimeUtils
+                .getClasspathFile(TeamToolsVersionManager.class);
+
+        // if we were unable to find the classpath, abort
+        if (selfClasspath == null) {
+            return null;
+
+        } else if (selfClasspath.isDirectory()) {
+            // if the classpath is a directory, we're in development mode.
+            // Return the "dist" directory that appears nearby
+            return new File(selfClasspath.getParentFile(), "dist");
+
+        } else {
+            // when the classpath is a JAR file, return the parent dir
+            return selfClasspath.getParentFile();
+        }
+    }
+
+    private static String checkAcceptableTeamToolsJar(File jarFile,
+            String dataVersion) {
+        try {
+            // only scan files whose names match a WBS Editor JAR pattern
+            String name = jarFile.getName().toLowerCase();
+            if (!name.startsWith("teamtools") && !name.startsWith("wbseditor"))
+                return null;
+            if (!name.endsWith(".jar"))
+                return null;
+
+            // read the attributes from the JAR file
+            JarFile jar = new JarFile(jarFile);
+            Attributes attrs = jar.getManifest().getMainAttributes();
+            jar.close();
+
+            // make sure this is a WBS Editor JAR file
+            String packageId = attrs.getValue("Dash-Pkg-ID");
+            if (packageId == null || !packageId.startsWith("teamTools"))
+                return null;
+
+            // if this JAR file only handles project data after a certain min
+            // version, ensure the current project satisfies that requirement
+            String minDataVersion = attrs.getValue(DATA_REQ_MANIFEST_ATTR);
+            if (minDataVersion != null
+                    && compareVersions(dataVersion, minDataVersion) < 0)
+                return null;
+
+            // all requirements seem to be met. Return the JAR version number
+            return attrs.getValue("Dash-Pkg-Version");
+
+        } catch (IOException e) {
+            return null;
+        }
     }
 
 }
