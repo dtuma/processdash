812e01fd894d242a6cb0ebc8af4eadf458e55f55
David Tuma <processdash@tuma-solutions.com>
2021-02-27 14:13:25 -0700

    Search additional places for a Planning Accelerator license

--- teamdash/src/teamdash/license/StandaloneLicenseManager.java
+++ teamdash/src/teamdash/license/StandaloneLicenseManager.java
@@ -23,26 +23,81 @@
 
 package teamdash.license;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 
+import net.sourceforge.processdash.tool.bridge.client.DirectoryPreferences;
 import net.sourceforge.processdash.util.FileUtils;
+import net.sourceforge.processdash.util.RuntimeUtils;
 
 public class StandaloneLicenseManager {
 
     public static StandaloneLicense getLicense() {
+        // try loading a license file from the global application directory
+        File licenseFile = getGlobalLicenseFile();
+        StandaloneLicense globalLicense = null;
         try {
+            globalLicense = loadLicense(new FileInputStream(licenseFile));
+        } catch (Exception e) {
+        }
+
+        // if a valid, non-expired global license was found, return it
+        if (globalLicense != null && !globalLicense.isExpired())
+            return globalLicense;
+
+        // try loading a bundled license file
+        StandaloneLicense bundledLicense = null;
+        try {
+            // look for a license inside this JAR file
             InputStream licenseData = StandaloneLicenseManager.class
                     .getResourceAsStream(LICENSE_PATH);
-            String xml = new String(FileUtils.slurpContents(licenseData, true),
-                    "UTF-8");
-            StandaloneLicense license = new StandaloneLicense(xml);
-            if (license.isValid())
-                return license;
+
+            // if the JAR didn't contain a license, look in the directory
+            // containing the JAR
+            if (licenseData == null)
+                licenseData = new FileInputStream(getAdjacentLicenseFile());
+
+            bundledLicense = loadLicense(licenseData);
 
         } catch (Exception e) {
         }
+
+        if (bundledLicense != null)
+            // if a valid bundled license was found, return it
+            return bundledLicense;
+        else if (globalLicense != null)
+            // otherwise, return the (expired) global license
+            return globalLicense;
+
+        // no valid licenses were found.
         return null;
+    }
+
+    private static File getGlobalLicenseFile() {
+        File appBaseDirectory = DirectoryPreferences.getApplicationDirectory();
+        File licenseDirectory = new File(appBaseDirectory, APP_LICENSE_DIR);
+        return new File(licenseDirectory, LICENSE_FILE);
+    }
+
+    private static File getAdjacentLicenseFile() {
+        File selfClasspath = RuntimeUtils
+                .getClasspathFile(StandaloneLicenseManager.class);
+        File installationDir = selfClasspath.getParentFile();
+        return new File(installationDir, LICENSE_FILE);
+    }
+
+    private static StandaloneLicense loadLicense(InputStream licenseData)
+            throws IOException, StandaloneLicenseException {
+        String xml = new String(FileUtils.slurpContents(licenseData, true),
+                "UTF-8");
+        StandaloneLicense license = new StandaloneLicense(xml);
+        if (license.isValid())
+            return license;
+        else
+            return null;
     }
 
     public static boolean isStandaloneJar() {
@@ -53,5 +108,7 @@ public class StandaloneLicenseManager {
     private static final String LICENSE_FILE = "planning-accelerator-license.xml";
 
     private static final String LICENSE_PATH = "/" + LICENSE_FILE;
+
+    private static final String APP_LICENSE_DIR = "licenseData";
 
 }
