1c9d3056e9a45c8195b0a26e80bd27b07e637f5b
David Tuma <processdash@tuma-solutions.com>
2023-08-02 12:46:56 -0600

    Don't crash redaction operation if data files are missing

--- src/net/sourceforge/processdash/tool/redact/HierarchyInfo.java
+++ src/net/sourceforge/processdash/tool/redact/HierarchyInfo.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2012-2017 Tuma Solutions, LLC
+// Copyright (C) 2012-2023 Tuma Solutions, LLC
 // Process Dashboard - Data Automation Tool for high-maturity processes
 //
 // This program is free software; you can redistribute it and/or
@@ -224,6 +224,8 @@ public class HierarchyInfo {
     private void scanDataFile(RedactFilterData data, Node node)
             throws IOException {
         BufferedReader in = data.getFile(node.dataFile);
+        if (in == null)
+            return;
         String line;
         int pos;
         while ((line = in.readLine()) != null) {
