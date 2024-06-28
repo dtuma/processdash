8d67e8d94afdd961b4217206335d4048f84116aa
David Tuma <processdash@tuma-solutions.com>
2024-06-28 09:28:12 -0600

    Write componentTime attrs to projDump for time on leaf components

--- src/net/sourceforge/processdash/tool/redact/filter/FilterWbsProjDump.java
+++ src/net/sourceforge/processdash/tool/redact/filter/FilterWbsProjDump.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2012-2020 Tuma Solutions, LLC
+// Copyright (C) 2012-2024 Tuma Solutions, LLC
 // Process Dashboard - Data Automation Tool for high-maturity processes
 //
 // This program is free software; you can redistribute it and/or
@@ -201,6 +201,7 @@ public class FilterWbsProjDump extends AbstractLineBasedFilter {
             line = replaceXmlAttr(line, "time", timeListMapper);
             line = replaceXmlAttr(line, "syncTime", timeListMapper);
             line = replaceXmlAttr(line, "deferredTime", timeListMapper);
+            line = replaceXmlAttr(line, "componentTime", timeListMapper);
             line = replaceXmlAttr(line, "cid", clientIdMapper);
         }
         if (filterExtLinks)
